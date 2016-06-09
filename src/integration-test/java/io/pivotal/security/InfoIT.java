869cbb902c11e63ded5556f2a8d8e2f8152c8912
Nitya Dhanushkodi <ndhanushkodi@pivotal.io>
2016-06-09 16:12:12 -0700

    Add a app jar integration test
    Add an integration test that launches the built jar as a child process,
    and verifies the info endpoint
    
    [#120376739] User should be able to see CM server build when checking version
    
    Signed-off-by: Philip Kuryloski <pkuryloski@pivotal.io>
    Signed-off-by: Nitya Dhanushkodi <ndhanushkodi@pivotal.io>

---
+++ src/integration-test/java/io/pivotal/security/InfoIT.java
@@ -0,0 +1,54 @@
+package io.pivotal.security;
+
+import com.greghaskins.spectrum.Spectrum;
+import org.junit.runner.RunWith;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import static com.greghaskins.spectrum.Spectrum.afterEach;
+import static com.greghaskins.spectrum.Spectrum.beforeEach;
+import static com.greghaskins.spectrum.Spectrum.describe;
+import static com.greghaskins.spectrum.Spectrum.it;
+import static com.greghaskins.spectrum.Spectrum.value;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+@RunWith(Spectrum.class)
+public class InfoIT {
+
+  private String APP_URL = "http://localhost:8080";
+  private final RestTemplate restTemplate = new RestTemplate();
+  private Spectrum.Value<Process> server = value(Process.class);
+
+  {
+    beforeEach(() -> {
+      ProcessBuilder processBuilder = new ProcessBuilder()
+          .command("java", "-jar", Helper.jarPath());
+
+      server.value = processBuilder.start();
+
+      Helper.waitForServer(APP_URL);
+    });
+
+    describe("the info endpoint", () -> {
+      it("respondns with the app version", () -> {
+        final ResponseEntity<String> response = restTemplate.getForEntity(APP_URL + "/info", String
+            .class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+        assertThat(response.getBody(), equalTo("{" +
+            "\"app\":{" +
+            "\"name\":\"Pivotal Credential Manager\"," +
+            "\"version\":\"0.1.0\"" +
+            "}" +
+            "}"));
+      });
+    });
+
+    afterEach(() -> {
+      server.value.destroy();
+    });
+  }
+
+}
