16262bd6938f61ebe97a0ad464558a23eb44c7b0
Larry Hamel <lhamel@pivotal.io>
2016-07-05 16:36:05 -0700

    change comparison of json to ignore child ordering
    [#125005931] fix red integration test resulting from json child order equality

--- src/integration-test/java/io/pivotal/security/InfoIT.java
+++ src/integration-test/java/io/pivotal/security/InfoIT.java
@@ -1,20 +1,17 @@
 package io.pivotal.security;
 
 import com.greghaskins.spectrum.Spectrum;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.runner.RunWith;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import static com.greghaskins.spectrum.Spectrum.afterEach;
-import static com.greghaskins.spectrum.Spectrum.beforeEach;
-import static com.greghaskins.spectrum.Spectrum.describe;
-import static com.greghaskins.spectrum.Spectrum.it;
-import static com.greghaskins.spectrum.Spectrum.value;
+import java.util.Optional;
+
+import static com.greghaskins.spectrum.Spectrum.*;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
-
-import java.util.Optional;
 
 @RunWith(Spectrum.class)
 public class InfoIT {
@@ -35,18 +32,19 @@ public class InfoIT {
 
     describe("the info endpoint", () -> {
       it("responds with the app version", () -> {
-        final ResponseEntity<String> response = restTemplate.getForEntity(APP_URL + "/info", String
-            .class);
-
         final String buildNumber = Optional.ofNullable(System.getenv("BUILD_NUMBER")).orElse("DEV");
-
-        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
-        assertThat(response.getBody(), equalTo("{" +
-            "\"app\":{" +
-            "\"name\":\"Pivotal Credential Manager\"," +
+        Object expected = JsonPath.parse("{" +
+            "\"app\":" +
+            "{\"name\":\"Pivotal Credential Manager\"," +
             "\"version\":\"0.1.0 build " + buildNumber + "\"" +
             "}" +
-            "}"));
+            "}")
+            .json();
+
+        final ResponseEntity<String> response = restTemplate.getForEntity(APP_URL + "/info", String.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+        assertThat(JsonPath.parse(response.getBody()).json(), equalTo(expected));
       });
     });
 
