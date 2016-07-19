15720005ff0245eaf7de8d7fac20b9ced72c352f
Rick Kawala <rkawala@pivotal.io>
2016-07-19 15:16:36 -0700

    Remove the unnecessary auth-server.client property
    [#118146033] CredHub should use UAA as identity store
    
    Signed-off-by: Philip Kuryloski <pkuryloski@pivotal.io>

--- src/main/java/io/pivotal/security/config/AuthServerProperties.java
+++ src/main/java/io/pivotal/security/config/AuthServerProperties.java
@@ -5,10 +5,11 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MapPropertySource;
 
-import javax.annotation.PostConstruct;
-import javax.validation.constraints.NotNull;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.annotation.PostConstruct;
+import javax.validation.constraints.NotNull;
 
 @ConfigurationProperties("auth-server")
 public class AuthServerProperties {
@@ -19,8 +20,7 @@ public class AuthServerProperties {
   @NotNull(message = "The auth-server.url configuration property is required.")
   private String url;
 
-  @NotNull(message = "The auth-server.client configuration property is required.")
-  private String client;
+  private final String client = "credhub";
 
   public String getUrl() {
     return url;
@@ -34,15 +34,10 @@ public class AuthServerProperties {
     return client;
   }
 
-  public void setClient(String client) {
-    this.client = client;
-  }
-
   @PostConstruct
   public void init() {
     Map<String, Object> map = new HashMap<>();
     map.put("info.auth-server.url", getUrl());
-    map.put("info.auth-server.client", getClient());
     MapPropertySource propertySource = new MapPropertySource("auth-server", map);
     environment.getPropertySources().addFirst(propertySource);
   }
