1a9c62ff5115e7fca0c7d87edd884cc86aef1205
Larry Hamel <lhamel@pivotal.io>
2016-07-26 16:57:12 -0700

    Add postgres db support and dynamically interpret
    -db type in application
    
    [#118801575] User should be able to deploy CM with configuration for in-memory database
    
    Signed-off-by: Nitya Dhanushkodi <ndhanushkodi@pivotal.io>

--- src/main/java/io/pivotal/security/config/DataStorageProperties.java
+++ src/main/java/io/pivotal/security/config/DataStorageProperties.java
@@ -1,24 +1,37 @@
 package io.pivotal.security.config;
 
+import com.google.common.base.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MapPropertySource;
-
-import java.util.Collections;
 
 import javax.annotation.PostConstruct;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import java.util.HashMap;
+import java.util.Map;
 
+/** excluding DataSourceAutoConfiguration allows us to set dynamic Hibernate dialect
+at runtime, based on requested data-storage.type */
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @ConfigurationProperties("data-storage")
 public class DataStorageProperties {
   @Autowired
   ConfigurableEnvironment environment;
 
   @NotNull(message = "The data-storage.type configuration property is required.")
-  @Pattern(regexp = "in-memory", message = "The data-storage.type configuration property must be \"in-memory\".")
+  @Pattern(regexp = "in-memory|postgres", message = "The data-storage.type configuration " +
+      "property must be \"in-memory\" or \"postgres\".")
   private String type;
+
+  private String username;
+  private String password;
 
   public String getType() {
     return type;
@@ -30,7 +43,45 @@ public class DataStorageProperties {
 
   @PostConstruct
   public void init() {
-    MapPropertySource propertySource = new MapPropertySource("data-storage", Collections.singletonMap("data-storage.type", getType()));
+    Map<String, Object> map = new HashMap<>();
+    MapPropertySource propertySource = new MapPropertySource("data-storage", map);
+
+    if ("postgres".equals(getType())) {
+      if (Strings.isNullOrEmpty(getUsername())) {
+        MessageSourceAccessor messageSourceAccessor = new MessageSourceAccessor(messageSource());
+        throw new RuntimeException(messageSourceAccessor.getMessage("error.postgres_requires_credentials"));
+      }
+      map.put("spring.datasource.url", "jdbc:postgresql://localhost:5432/credhub");
+      map.put("spring.datasource.username", getUsername());
+      map.put("spring.datasource.password", getPassword());
+      map.put("spring.jpa.database-platform", "org.hibernate.dialect.PostgreSQLDialect");
+    } else if ("in-memory".equals(getType())) {
+      map.put("spring.jpa.database-platform", "org.hibernate.dialect.H2Dialect");
+    }
     environment.getPropertySources().addFirst(propertySource);
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public MessageSource messageSource() {
+    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+    messageSource.setBasenames("messages");
+    messageSource.setDefaultEncoding("UTF-8");
+    messageSource.setCacheSeconds(1);
+    return messageSource;
   }
 }
---
+++ src/test/java/io/pivotal/security/config/DataStoragePropertiesTest.java
@@ -0,0 +1,78 @@
+package io.pivotal.security.config;
+
+import com.greghaskins.spectrum.Spectrum;
+import io.pivotal.security.CredentialManagerApp;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import static com.greghaskins.spectrum.Spectrum.*;
+import static io.pivotal.security.helper.SpectrumHelper.autoTransactional;
+import static io.pivotal.security.helper.SpectrumHelper.wireAndUnwire;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.fail;
+
+@RunWith(Spectrum.class)
+@SpringApplicationConfiguration(classes = CredentialManagerApp.class)
+public class DataStoragePropertiesTest {
+  @Autowired
+  ConfigurableEnvironment environment;
+
+  private DataStorageProperties subject;
+
+  {
+    wireAndUnwire(this);
+    autoTransactional(this);
+
+    beforeEach(() -> {
+      subject = new DataStorageProperties();
+      subject.environment = environment;
+    });
+
+    describe("when using in-memory data storage", () -> {
+      it("sets the dialect to h2", () -> {
+        subject.setType("in-memory");
+
+        subject.init();
+
+        assertThat(environment.getProperty("spring.jpa.database-platform"), equalTo("org.hibernate.dialect.H2Dialect"));
+      });
+    });
+
+    describe("when using postgres data storage", () -> {
+      beforeEach(() -> {
+        subject.setType("postgres");
+        subject.setUsername("postgres");
+        subject.setPassword("postgres-password");
+      });
+      it("sets the url to point to the database", () -> {
+        subject.init();
+
+        assertThat(environment.getProperty("spring.datasource.url"), equalTo("jdbc:postgresql://localhost:5432/credhub"));
+      });
+      it("sets the username and password", () -> {
+        subject.init();
+
+        assertThat(environment.getProperty("spring.datasource.username"), equalTo("postgres"));
+        assertThat(environment.getProperty("spring.datasource.password"), equalTo("postgres-password"));
+      });
+      it("sets the dialect to postgres", () -> {
+        subject.init();
+
+        assertThat(environment.getProperty("spring.jpa.database-platform"), equalTo("org.hibernate.dialect.PostgreSQLDialect"));
+      });
+      it("when a username is missing", () -> {
+        try {
+          subject.setUsername("");
+          subject.init();
+          fail();
+        }
+        catch (RuntimeException r) {
+          assertThat(r.getMessage(), equalTo("Using a postgres database requires a username."));
+        }
+      });
+    });
+  }
+}
