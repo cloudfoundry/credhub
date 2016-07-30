91cee2ae5e7a1d26cb0eacdb004ae48dba143912
Larry Hamel <lhamel@pivotal.io>
2016-07-29 17:33:35 -0700

    Add extra notes for FakeAuditLogInterceptor
    Intellij marks it as unused, even though it really is used
    
    [finishes #124008491] Auditor should see a log for each time a credential is set
    
    Signed-off-by: Philip Kuryloski <pkuryloski@pivotal.io>

--- src/test/java/io/pivotal/security/interceptor/FakeAuditLogInterceptor.java
+++ src/test/java/io/pivotal/security/interceptor/FakeAuditLogInterceptor.java
@@ -6,8 +6,10 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+@SuppressWarnings("unused")
 @Component
 @Primary
 @ConditionalOnMissingBean(AuditLogConfigurationTest.TestConfiguration.class)
 public class FakeAuditLogInterceptor extends HandlerInterceptorAdapter implements AuditLogInterceptor {
+  // This test-only-bean is used to disable audit log database hits to speed up tests
 }
