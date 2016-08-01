3417ca889f6aec0ffe88ea969b304f8ee01dec8a
Philip Kuryloski <pkuryloski@pivotal.io>
2016-08-01 09:02:30 -0700

    Fix AuditLogInterceptor configuration in test
    [#126917375] Requests should fail if log cannot be written to db

--- src/test/java/io/pivotal/security/interceptor/FakeAuditLogInterceptor.java
+++ src/test/java/io/pivotal/security/interceptor/FakeAuditLogInterceptor.java
@@ -1,7 +1,6 @@
 package io.pivotal.security.interceptor;
 
-import io.pivotal.security.config.AuditLogConfigurationTest;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -9,7 +8,7 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 @SuppressWarnings("unused")
 @Component
 @Primary
-@ConditionalOnMissingBean(AuditLogConfigurationTest.TestConfiguration.class)
+@ConditionalOnExpression("#{!environment.getProperty('spring.profiles.active').contains('AuditLogConfigurationTest')}")
 public class FakeAuditLogInterceptor extends HandlerInterceptorAdapter implements AuditLogInterceptor {
   // This test-only-bean is used to disable audit log database hits to speed up tests
 }
