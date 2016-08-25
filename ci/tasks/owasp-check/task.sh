b71a7cc2f2484a58ee8624866e33c2ecd2989027
Stu Pollock <spollock@pivotal.io>
2016-08-25 16:50:07 -0700

    Less build noise
    Signed-off-by: Philip Kuryloski <pkuryloski@pivotal.io>

--- ci/tasks/owasp-check/task.sh
+++ ci/tasks/owasp-check/task.sh
@@ -8,7 +8,7 @@ export TERM=xterm
 APP_DIR=sec-eng-credential-manager
 
 pushd $APP_DIR
-gradle clean dependencyCheck --info
+gradle clean dependencyCheck
 popd
 
 echo "Credential Manager owasp check results" > ${OUTPUT_PATH}/owasp-check-email-subject.txt
