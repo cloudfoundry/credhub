210c911ed5b1def639408db2b90d53c69f13b331
Rick Kawala <rkawala@pivotal.io>
2016-09-02 17:12:07 -0700

    Make Flyway work with the (older) version of Postgres that BOSH uses
    [#128604343] User should be able to install new version of CredHub with existing database
    
    Signed-off-by: Iryna Shustava <ishustava@pivotal.io>

--- src/main/resources/db/migration/postgres/V1__initial.sql
+++ src/main/resources/db/migration/postgres/V1__initial.sql
@@ -6,26 +6,10 @@
 -- Dumped by pg_dump version 9.5.2
 
 SET statement_timeout = 0;
-SET lock_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
-SET row_security = off;
-
---
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
 
 SET search_path = public, pg_catalog;
 
