3c117842a2591cb981f52afac302946352c0ba89
Iryna Shustava <ishustava@pivotal.io>
2016-09-26 09:49:12 -0700

    Make DevKeyProvider bean creation conditional on encryption.provider property
    [#130498465] Deployment should fail if provided dev_internal key is invalid
    
    Signed-off-by: Stu Pollock <spollock@pivotal.io>

--- src/main/java/io/pivotal/security/config/DevKeyProvider.java
+++ src/main/java/io/pivotal/security/config/DevKeyProvider.java
@@ -1,9 +1,11 @@
 package io.pivotal.security.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConditionalOnProperty(value = "encryption.provider", havingValue = "dev_internal")
 public class DevKeyProvider {
   @Value("${encryption.dev-key}")
   private String devKey;
