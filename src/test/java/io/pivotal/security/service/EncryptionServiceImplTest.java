d6e98377d78d51342c1dfdba1af57ceb004fbbcd
Phil Goodwin <pgoodwin@pivotal.io>
2016-10-06 14:29:24 -0700

    Repair tests against postgres by not starting Spring contexts
    [#131599351] for 'find all paths' run automated migration test against mysql and postgres
    
    Signed-off-by: Stu Pollock <spollock@pivotal.io>

--- src/test/java/io/pivotal/security/service/EncryptionServiceImplTest.java
+++ src/test/java/io/pivotal/security/service/EncryptionServiceImplTest.java
@@ -1,50 +1,33 @@
 package io.pivotal.security.service;
 
 import com.greghaskins.spectrum.Spectrum;
-import io.pivotal.security.CredentialManagerApp;
-import io.pivotal.security.CredentialManagerTestContextBootstrapper;
+import io.pivotal.security.config.DevKeyProvider;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.BootstrapWith;
 
 import static com.greghaskins.spectrum.Spectrum.beforeEach;
 import static com.greghaskins.spectrum.Spectrum.it;
-import static io.pivotal.security.helper.SpectrumHelper.wireAndUnwire;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
-import java.security.Provider;
-
 @RunWith(Spectrum.class)
-@SpringApplicationConfiguration(classes = CredentialManagerApp.class)
-@BootstrapWith(CredentialManagerTestContextBootstrapper.class)
-@ActiveProfiles({"unit-test", "EncryptionServiceImplTest"})
 public class EncryptionServiceImplTest {
 
   private final String plaintext = "this is a string";
 
-  @Autowired
   BCEncryptionConfiguration bcEncryptionConfiguration;
-
-  @Autowired
-  @InjectMocks
   EncryptionServiceImpl subject;
-
-  @Mock
-  Provider provider;
 
   private EncryptionService.Encryption encryption;
 
   {
-    wireAndUnwire(this);
-
     beforeEach(() -> {
+      bcEncryptionConfiguration = new BCEncryptionConfiguration();
+      bcEncryptionConfiguration.devKeyProvider = new DevKeyProvider() {
+        @Override
+        public String getDevKey() {
+          return "1234abcd1234abcd1234abcd1234abcd";
+        }
+      };
       subject = new EncryptionServiceImpl(bcEncryptionConfiguration);
       encryption = subject.encrypt(plaintext);
     });
