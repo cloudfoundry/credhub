d14af11022f651deff633054fce1792fca56ad82
Iryna Shustava <ishustava@pivotal.io>
2016-11-08 12:34:39 -0800

    Generate random uuid for every row
    [#133706515] Existing CAs have null uuid in the database

--- src/main/java/db/migration/common/V11_1__set_uuid_in_named_certificate_authority_where_null.java
+++ src/main/java/db/migration/common/V11_1__set_uuid_in_named_certificate_authority_where_null.java
@@ -3,12 +3,19 @@ package db.migration.common;
 import org.flywaydb.core.api.migration.spring.SpringJdbcMigration;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.util.List;
 import java.util.UUID;
 
 public class V11_1__set_uuid_in_named_certificate_authority_where_null implements SpringJdbcMigration {
   public void migrate(JdbcTemplate jdbcTemplate) throws Exception {
-    jdbcTemplate.update(
-        "update named_certificate_authority set uuid = ? where uuid is null",
-        UUID.randomUUID().toString());
+    List<Long> nullUuidRecords = jdbcTemplate.queryForList(
+        "select id from named_certificate_authority where uuid is null",
+        Long.class);
+
+    for (Long record : nullUuidRecords) {
+      jdbcTemplate.update(
+          "update named_certificate_authority set uuid = ? where id = ?",
+          UUID.randomUUID().toString(), record);
+    }
   }
 }
