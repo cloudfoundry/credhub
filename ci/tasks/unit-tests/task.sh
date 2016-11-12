60048e87f8b9be4105bc2a58f8bb74db119d60b7
Sebastian Wittenkamp <swittenkamp@pivotal.io>
2016-11-11 17:39:17 -0800

    Bail if mysql fails to start
    Periodically, MySQL will fail to start. The service command appears to
    still exit 0 when this happens. This means that when we try to create
    the test db, it will hang for a long time before finally timing out.
    
    With this change, we check status explicitly via the mysqladmin tool and
    blow up if we failed to connect. This should help the pipeline fail fast
    if there's an issue.
    
    Also, supply the --no-daemon flag to gradle since we don't ever need to
    start the daemon in the pipeline. This should help shave a little bit of
    time off the test run.
    
    [#133943771] Unit tests intermittently run forever

--- ci/tasks/unit-tests/task.sh
+++ ci/tasks/unit-tests/task.sh
@@ -6,6 +6,7 @@ set -o pipefail
 export TERM=xterm
 
 service mysql start
+mysqladmin -u root status # bail out quickly if mysql failed to start
 mysql -u root -e "create database credhub_test"
 
 service postgresql start
@@ -15,10 +16,11 @@ psql -d credhub_test -c "ALTER USER root WITH PASSWORD 'root';"
 
 pushd sec-eng-credential-manager
 set +e
-gradle -Dspring.profiles.active=${DATABASE_PROFILE} clean test
+gradle --no-daemon -Dspring.profiles.active=${DATABASE_PROFILE} clean test
 exit_code=$?
 popd
 
 service mysql stop
+service postgresql stop
 
 exit $exit_code
