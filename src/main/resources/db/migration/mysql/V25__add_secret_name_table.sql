12169075db36f37f067377c309b24d7632a5cac0
Kelly Gerritz <kgerritz@pivotal.io>
2017-02-10 11:15:46 -0800

    Specify secret_name table encoding for mysql
    Mariadb requires the right type of utf-8 encoding or the migrations will
    fail.
    
    [#137494995] `--no-overwrite` can overwrite
    
    Signed-off-by: Isobel Redelmeier <iredelmeier@pivotal.io>

--- src/main/resources/db/migration/mysql/V25__add_secret_name_table.sql
+++ src/main/resources/db/migration/mysql/V25__add_secret_name_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE secret_name (
   uuid BINARY(16) NOT NULL,
   name VARCHAR(255) NOT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE secret_name
   ADD CONSTRAINT secret_name_pkey PRIMARY KEY(uuid);
