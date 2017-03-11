64e4e3d68b3c6500ec964d062c6f9961a1a89092
Iryna Shustava <ishustava@pivotal.io>
2017-03-10 17:41:48 -0800

    setup_dev_mtls.sh doesn't generate client certs
    [#138290503] Client cert validity dates should be validated for MTLS authentication
    
    Signed-off-by: Phil Goodwin <pgoodwin@pivotal.io>

--- setup_dev_mtls.sh
+++ setup_dev_mtls.sh
@@ -4,10 +4,11 @@ set -eu
 
 DIRNAME=$(dirname "$0")
 
-PASSWORD=changeit
+KEYSTORE_PASSWORD=changeit
 
-DNAME='CN=localhost'
-DNAME_CA="CN=credhub_test"
+SERVER_DNAME='localhost'
+CLIENT_DNAME='credhub_client'
+CA_DNAME="credhub_client_ca"
 
 CA_NAME=ca
 CLIENT_NAME=client
@@ -21,70 +22,47 @@ clean() {
 }
 
 setup_tls_key_store() {
-    echo "Generating a key store for TLS..."
-	keytool -genkey -alias cert \
-	    -keyalg RSA -keysize 4096 -sigalg SHA512withRSA -keypass ${PASSWORD} \
-	    -validity 365 -dname ${DNAME} \
-	    -keystore ${KEY_STORE} -storepass ${PASSWORD}
+    echo "Generating a key store for the certificate the server presents during TLS"
+    # generate keypair for the server cert
+	openssl genrsa -out server_key.pem 2048
+
+	echo "Create CSR for the server cert"
+    openssl req -new -sha256 -key server_key.pem -subj "/CN=${SERVER_DNAME}" -out server.csr
+
+    echo "Generate server certificate signed by our CA"
+    openssl x509 -req -in server.csr -CA ${CA_NAME}.pem -CAkey ${CA_NAME}_key.pem \
+        -CAcreateserial -out server.pem
+
+    echo "Create a .p12 file that contains both server cert and private key"
+    openssl pkcs12 -export -in server.pem -inkey server_key.pem \
+        -out server.p12 -name cert -password pass:changeit
+
+    echo "Import signed certificate into the keystore"
+	keytool -importkeystore \
+        -srckeystore server.p12 -srcstoretype PKCS12 -srcstorepass changeit \
+        -deststorepass ${KEYSTORE_PASSWORD} -destkeypass ${KEYSTORE_PASSWORD} \
+        -destkeystore ${KEY_STORE} -alias cert
+
+    rm server.p12 server.csr
 }
 
 generate_ca() {
-    echo "Generating self signed CA..."
+    echo "Generating root CA for both client and server certificates into ${CA_NAME}.pem and ${CA_NAME}_key.pem"
     openssl req \
       -x509 \
       -newkey rsa:2048 \
       -days 30 \
       -sha256 \
       -nodes \
-      -subj "/CN=${DNAME_CA}" \
-      -keyout ${CA_NAME}_key.pem\
+      -subj "/CN=${CA_DNAME}" \
+      -keyout ${CA_NAME}_key.pem \
       -out ${CA_NAME}.pem
 }
 
-add_ca_to_trusted_keystore() {
-    echo "Adding CA to trust store for mTLS..."
+add_ca_to_truststore() {
+    echo "Adding root CA to servers trust store for mTLS..."
     keytool -import -trustcacerts -noprompt -alias ${CA_NAME} -file ${CA_NAME}.pem \
-	    -keystore ${TRUST_STORE} -storepass ${PASSWORD}
-}
-
-generate_client_certs() {
-    echo "Generating client certificate signed by the trusted CA..."
-    # generate keypair
-    openssl genrsa -out ${CLIENT_NAME}_key.pem 2048
-
-    # create valid CSR
-    openssl req -new -key ${CLIENT_NAME}_key.pem -out client.csr -subj "/CN=${DNAME}"
-
-    # create expired CSR
-    openssl req -new -key ${CLIENT_NAME}_key.pem -out expired_client.csr -subj "/CN=${DNAME}"
-
-    # create valid client certificate signed by the trusted CA
-    openssl x509 \
-        -req \
-        -in client.csr \
-        -CA ${CA_NAME}.pem \
-        -CAkey ${CA_NAME}_key.pem \
-        -CAcreateserial \
-        -out ${CLIENT_NAME}.pem \
-        -days 30 \
-        -sha256
-
-    # create expired client certificate signed by the trusted CA
-    openssl x509 \
-        -req \
-        -in expired_client.csr \
-        -CA ${CA_NAME}.pem \
-        -CAkey ${CA_NAME}_key.pem \
-        -CAserial ca.srl \
-        -out expired_${CLIENT_NAME}.pem \
-        -days -30 \
-        -sha256
-}
-
-generate_server_cert(){
-    echo "Generating Server Cert"
-    keytool -certreq -alias cert -file server.csr -keystore ${KEY_STORE} -storepass changeit
-	openssl x509 -req -in server.csr -CA ca.pem -CAkey ca_key.pem -CAserial ca.srl -out server.pem
+	    -keystore ${TRUST_STORE} -storepass ${KEYSTORE_PASSWORD}
 }
 
 pushd ${DIRNAME}/src/test/resources >/dev/null
@@ -92,11 +70,9 @@ pushd ${DIRNAME}/src/test/resources >/dev/null
         echo "Key store and trust store are already set up!"
     else
         clean
-        setup_tls_key_store
         generate_ca
-        add_ca_to_trusted_keystore
-        generate_client_certs
-        generate_server_cert
+        add_ca_to_truststore
+        setup_tls_key_store
 
         echo "Finished setting up key stores for TLS and mTLS!"
         echo e.g., curl -H \"Content-Type: application/json\" \
