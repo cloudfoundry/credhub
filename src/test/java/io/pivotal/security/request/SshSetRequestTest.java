36258a122b3fba46426c29d11aa4638b5fd7a961
Edie Beer <ebeer@pivotal.io>
2017-03-13 10:08:45 -0400

    Add missing test
    [#140581861] Use Jackson for rsa/ssh set requests
    
    Signed-off-by: Andrew Cantino <acantino@pivotal.io>

---
+++ src/test/java/io/pivotal/security/request/SshSetRequestTest.java
@@ -0,0 +1,94 @@
+package io.pivotal.security.request;
+
+import com.greghaskins.spectrum.Spectrum;
+import org.junit.runner.RunWith;
+
+import javax.validation.ConstraintViolation;
+import java.util.Set;
+
+import static com.greghaskins.spectrum.Spectrum.describe;
+import static com.greghaskins.spectrum.Spectrum.it;
+import static io.pivotal.security.helper.JsonHelper.deserialize;
+import static io.pivotal.security.helper.JsonHelper.deserializeAndValidate;
+import static io.pivotal.security.helper.JsonHelper.hasViolationWithMessage;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
+
+@RunWith(Spectrum.class)
+public class SshSetRequestTest {
+  {
+    describe("when the value is valid", () -> {
+      it("should not have violations", () -> {
+        String json = "{" +
+            "\"name\": \"/example/ssh\"," +
+            "\"type\": \"ssh\"," +
+            "\"value\": {" +
+              "\"public_key\":\"fake-public-key\"," +
+              "\"private_key\":\"fake-private-key\"" +
+            "}" +
+          "}";
+        Set<ConstraintViolation<BaseSecretSetRequest>> violations = deserializeAndValidate(json, BaseSecretSetRequest.class);
+
+        assertThat(violations.size(), equalTo(0));
+      });
+
+      it("should should deserialize to a SshSetRequest", () -> {
+        String json = "{" +
+            "\"name\": \"/example/ssh\"," +
+            "\"type\": \"ssh\"," +
+            "\"value\": {" +
+              "\"public_key\":\"fake-public-key\"," +
+              "\"private_key\":\"fake-private-key\"" +
+            "}" +
+          "}";
+        BaseSecretSetRequest deserialize = deserialize(json, BaseSecretSetRequest.class);
+
+        assertThat(deserialize, instanceOf(SshSetRequest.class));
+
+      });
+    });
+
+    describe("when no value is set", () -> {
+      it("should be in invalid", () -> {
+        String json = "{\n" +
+          "  \"name\": \"/example/ssh\",\n" +
+          "  \"type\": \"ssh\"\n" +
+          "}";
+        Set<ConstraintViolation<BaseSecretSetRequest>> violations = deserializeAndValidate(json, BaseSecretSetRequest.class);
+
+        assertThat(violations, contains(hasViolationWithMessage("error.missing_value")));
+      });
+    });
+
+    describe("when value is an empty object", () -> {
+      it("should be invalid", () -> {
+        String json = "{\n" +
+          "  \"name\": \"/example/ssh\",\n" +
+          "  \"type\": \"ssh\",\n" +
+          "  \"value\": {}\n" +
+          "}";
+        Set<ConstraintViolation<BaseSecretSetRequest>> violations = deserializeAndValidate(json, BaseSecretSetRequest.class);
+
+        assertThat(violations, contains(hasViolationWithMessage("error.missing_rsa_ssh_parameters")));
+      });
+    });
+
+    describe("when ssh has all null sub-fields", () -> {
+      it("should be invalid", () -> {
+        String json = "{\n" +
+          "  \"name\": \"/example/ssh\",\n" +
+          "  \"type\": \"ssh\",\n" +
+          "  \"value\": {" +
+          "    \"public_key\":\"\"," +
+          "    \"private_key\":\"\"" +
+          "  }" +
+          "}";
+        Set<ConstraintViolation<BaseSecretSetRequest>> violations = deserializeAndValidate(json, BaseSecretSetRequest.class);
+
+        assertThat(violations, contains(hasViolationWithMessage("error.missing_rsa_ssh_parameters")));
+      });
+    });
+  }
+}
