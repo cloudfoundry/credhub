c2eac8116592e7c8c819c6d9064cfc6776e0874e
Jacques Chester <jchester@pivotal.io>
2017-03-13 11:37:55 -0400

    Extract getKeyLength to NamedRsaSecretHelper
    [#141513753] Extract getKeyLength from NamedRsaSecretData to utility used by Domain object and test it

--- src/main/java/io/pivotal/security/entity/NamedRsaSecretData.java
+++ src/main/java/io/pivotal/security/entity/NamedRsaSecretData.java
@@ -1,19 +1,12 @@
 package io.pivotal.security.entity;
 
 import io.pivotal.security.view.SecretKind;
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.lang.StringUtils;
 
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.SecondaryTable;
-import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
-import java.security.interfaces.RSAPublicKey;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.X509EncodedKeySpec;
 
 @Entity
 @DiscriminatorValue(NamedRsaSecretData.SECRET_TYPE)
@@ -22,9 +15,6 @@ import java.security.spec.X509EncodedKeySpec;
   pkJoinColumns = { @PrimaryKeyJoinColumn(name = "uuid", referencedColumnName = "uuid") }
 )
 public class NamedRsaSecretData extends NamedSecretData<NamedRsaSecretData> {
-  static private final String RSA_START = "-----BEGIN PUBLIC KEY-----\n";
-  static private final String RSA_END = "\n-----END PUBLIC KEY-----";
-  static private final String NEW_LINE = "\n";
   static final String TABLE_NAME = "RsaSecret";
   public static final String SECRET_TYPE = "rsa";
 
@@ -63,23 +53,7 @@ public class NamedRsaSecretData extends NamedSecretData<NamedRsaSecretData> {
   }
 
   public int getKeyLength() {
-    String publicKey = this.getPublicKey();
-
-    if (StringUtils.isEmpty(publicKey)) {
-      return 0;
-    }
-
-    try {
-      String key = publicKey
-            .replaceFirst(RSA_START, "")
-            .replaceFirst(RSA_END, "")
-            .replaceAll(NEW_LINE, "");
-      byte[] byteKey = Base64.decodeBase64(key.getBytes());
-      X509EncodedKeySpec X509publicKey = new X509EncodedKeySpec(byteKey);
-      KeyFactory kf = KeyFactory.getInstance("RSA");
-      return ((RSAPublicKey) kf.generatePublic(X509publicKey)).getModulus().bitLength();
-    } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-      throw new RuntimeException(e);
-    }
+    final NamedRsaSecretHelper namedRsaSecretHelper = new NamedRsaSecretHelper(this);
+    return namedRsaSecretHelper.getKeyLength();
   }
 }
---
+++ src/main/java/io/pivotal/security/entity/NamedRsaSecretHelper.java
@@ -0,0 +1,43 @@
+package io.pivotal.security.entity;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang.StringUtils;
+
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+
+public class NamedRsaSecretHelper {
+  static private final String RSA_START = "-----BEGIN PUBLIC KEY-----\n";
+  static private final String RSA_END = "\n-----END PUBLIC KEY-----";
+  static private final String NEW_LINE = "\n";
+
+  private final NamedRsaSecretData namedRsaSecretData;
+
+  public NamedRsaSecretHelper(NamedRsaSecretData namedRsaSecretData) {
+    this.namedRsaSecretData = namedRsaSecretData;
+  }
+
+  public int getKeyLength() {
+    String publicKey = namedRsaSecretData.getPublicKey();
+
+    if (StringUtils.isEmpty(publicKey)) {
+      return 0;
+    }
+
+    try {
+      String key = publicKey
+        .replaceFirst(RSA_START, "")
+        .replaceFirst(RSA_END, "")
+        .replaceAll(NEW_LINE, "");
+      byte[] byteKey = Base64.decodeBase64(key.getBytes());
+      X509EncodedKeySpec X509publicKey = new X509EncodedKeySpec(byteKey);
+      KeyFactory kf = KeyFactory.getInstance("RSA");
+      return ((RSAPublicKey) kf.generatePublic(X509publicKey)).getModulus().bitLength();
+    } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
