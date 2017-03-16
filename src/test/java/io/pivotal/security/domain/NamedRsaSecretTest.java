c82f7abdd468df8f989ac4a1997d8d2a6e538cb4
Ashwin Venkatesh <avenkatesh@pivotal.io>
2017-03-16 10:01:01 -0400

    Moved and added test for #getKeyLength from NamedRsaSecretTest to NamedRsaSecretHelperTest
    [#141513753] Extract getKeyLength from NamedRsaSecretData to utility used by Domain object and test it
    
    Signed-off-by: Edie Beer <ebeer@pivotal.io>

--- src/test/java/io/pivotal/security/domain/NamedRsaSecretTest.java
+++ src/test/java/io/pivotal/security/domain/NamedRsaSecretTest.java
@@ -35,32 +35,6 @@ public class NamedRsaSecretTest {
       assertThat(subject.getSecretType(), equalTo("rsa"));
     });
 
-    describe("#getKeyLength", () -> {
-      it("should return the length of the public key", () -> {
-        String publicKey = "-----BEGIN PUBLIC KEY-----\n" +
-            "MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAoRIqdibiYHKZhyH91xYR\n" +
-            "Tpz728+A8d/t2U2e8OIhNqI7pjh5uKnbmeoAXdZAbGN3TW7xArdMAUOSRhELH0Gc\n" +
-            "8XGz6ZnY+KGuTnmBO+ZamE3kltwqJBfxwV2UGV5bJIVVToLpLa1GDF4p7g8I8W/a\n" +
-            "KURKCgMNlRQw38Wi8yuEWyCHWHrqon8CcA5ovUg1pyrpR9i+5NTCRadDf1JIQfKB\n" +
-            "Mt/gA/s6+ZaWOB6mbWv67OUS5wHWe0tcX2g4KK3IDlkzKQulSHQoIPEf+7l+vJEJ\n" +
-            "KT+C2cI+pl/qLVtbY+jsNr8acxja0ri4pUGEQPKP5009qisloEDlQMb/gMT5aHoF\n" +
-            "8GORc1EloUG4CpnPUe0L63Q3uSZkLSPAiYqwCi7Wu/L7aVeynGk3CFIPALyh/hIi\n" +
-            "SCOX6Jc81o9hZLADEFx4o4qaK4/MQczLaPkESO2578MI+eNwV3d02CIaUeSzK91b\n" +
-            "ZlAsqUUXaxxOQ+0WcJpE1O+IUXoBJ7XSZAqfdogLVUM0A+wW8Duxthuh1j7z284B\n" +
-            "NjWi9nPZnD3KT0vLv8KbwrW0XgiMzsaAdZKlexKZQuuzAOVNHb0hd3H36lBqAOPg\n" +
-            "G0S+H7L3o8XAPcqkke2xs/tcfF05DX+kpD2xdeDWs9MK39FnGtYp8gTKoDkzf0vp\n" +
-            "o2oUFe5cAKZHziOqNuoc7SUCAwEAAQ==\n" +
-            "-----END PUBLIC KEY-----";
-        subject.setPublicKey(publicKey);
-
-        assertThat(subject.getKeyLength(), equalTo(4096));
-      });
-
-      it("should return 0 if the private key has not been set", () -> {
-        assertThat(subject.getKeyLength(), equalTo(0));
-      });
-    });
-
     describe("#copyInto", () -> {
       it("should copy the correct properties into the other object", () -> {
         Instant frozenTime = Instant.ofEpochSecond(1400000000L);
