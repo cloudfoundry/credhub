2184da3676e43a9720ac6455763e9bcf5dc3721c
Edie Beer <ebeer@pivotal.io>
2017-03-20 09:53:56 -0400

    Replace sun package with apache package
    [#142053223] Fix NotImplementedException warning
    
    Signed-off-by: Jacques Chester <jchester@pivotal.io>

--- src/main/java/io/pivotal/security/request/SecretRegenerateRequest.java
+++ src/main/java/io/pivotal/security/request/SecretRegenerateRequest.java
@@ -2,7 +2,7 @@ package io.pivotal.security.request;
 
 import io.pivotal.security.domain.Encryptor;
 import io.pivotal.security.domain.NamedSecret;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import org.apache.commons.lang.NotImplementedException;
 
 public class SecretRegenerateRequest extends BaseSecretPostRequest {
   @Override
