f355993b821d77c0f20dcdd3be10e0bf1a4f3a9a
Iryna Shustava <ishustava@pivotal.io>
2017-04-12 18:12:25 -0700

    Mockify AccessControlAuditingTest
    To eliminate database interference
    
    [#142087153] User should have read and write ACEs to credentials they have created via generate
    
    Signed-off-by: Joe Kirwin <jkirwin@pivotal.io>

--- src/test/java/io/pivotal/security/controller/v1/permissions/AccessControlAuditingTest.java
+++ src/test/java/io/pivotal/security/controller/v1/permissions/AccessControlAuditingTest.java
@@ -1,34 +1,40 @@
 package io.pivotal.security.controller.v1.permissions;
 
+import static io.pivotal.security.entity.AuditingOperationCode.ACL_ACCESS;
+import static io.pivotal.security.util.AuthConstants.UAA_OAUTH2_PASSWORD_GRANT_TOKEN;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import io.pivotal.security.CredentialManagerApp;
+import io.pivotal.security.data.AccessControlDataService;
 import io.pivotal.security.data.OperationAuditRecordDataService;
 import io.pivotal.security.entity.OperationAuditRecord;
+import io.pivotal.security.request.AccessControlEntry;
+import io.pivotal.security.request.AccessControlOperation;
+import io.pivotal.security.service.PermissionService;
 import io.pivotal.security.util.DatabaseProfileResolver;
+import java.util.Arrays;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
-import static io.pivotal.security.entity.AuditingOperationCode.ACL_ACCESS;
-import static io.pivotal.security.util.AuthConstants.UAA_OAUTH2_PASSWORD_GRANT_TOKEN;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsEqual.equalTo;
-import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.verify;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles(
@@ -38,11 +44,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(classes = CredentialManagerApp.class)
 public class AccessControlAuditingTest {
 
+  public static final String CRED1 = "/cred1";
+  public static final String TESTPASSWORD = "testpassword";
   @Autowired
   private WebApplicationContext applicationContext;
 
-  @SpyBean
+  @MockBean
   private OperationAuditRecordDataService operationAuditRecordDataService;
+
+  @MockBean
+  private AccessControlDataService accessControlDataService;
+
+  @MockBean
+  private PermissionService permissionService;
 
   private MockMvc mockMvc;
 
@@ -51,25 +65,16 @@ public class AccessControlAuditingTest {
     mockMvc = MockMvcBuilders.webAppContextSetup(applicationContext)
       .apply(springSecurity())
       .build();
-
-    MockHttpServletRequestBuilder put = put("/api/v1/data")
-      .header("Authorization", "Bearer " + UAA_OAUTH2_PASSWORD_GRANT_TOKEN)
-      .accept(APPLICATION_JSON)
-      .contentType(APPLICATION_JSON)
-      .content("{"
-        + "  \"name\": \"/cred1\","
-        + "  \"type\": \"password\","
-        + "  \"value\": \"testpassword\""
-        + "}");
-
-    this.mockMvc.perform(put)
-      .andExpect(status().isOk());
+    AccessControlEntry ace = new AccessControlEntry(
+        "uaa-user:df0c1a26-2875-4bf5-baf9-716c6bb5ea6d",
+        Arrays.asList(AccessControlOperation.READ_ACL));
+    when(accessControlDataService.getAccessControlList(eq(CRED1))).thenReturn(Arrays.asList(ace));
     reset(operationAuditRecordDataService);
   }
 
   @Test
   public void whenGettingAnAcl_itLogsTheRetrieval() throws Exception {
-    final MockHttpServletRequestBuilder get = get("/api/v1/acls?credential_name=/cred1")
+    final MockHttpServletRequestBuilder get = get("/api/v1/acls?credential_name=" + CRED1)
       .header("Authorization", "Bearer " + UAA_OAUTH2_PASSWORD_GRANT_TOKEN)
       .accept(APPLICATION_JSON)
       .contentType(APPLICATION_JSON);
@@ -83,7 +88,7 @@ public class AccessControlAuditingTest {
 
     OperationAuditRecord auditRecord = recordCaptor.getValue();
 
-    assertThat(auditRecord.getCredentialName(), equalTo("/cred1"));
+    assertThat(auditRecord.getCredentialName(), equalTo(CRED1));
     assertThat(auditRecord.getOperation(), equalTo(ACL_ACCESS.toString()));
   }
 }
