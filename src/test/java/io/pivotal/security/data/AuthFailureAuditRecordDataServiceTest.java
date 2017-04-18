2b8f77b0d03da2ace5feb51244d94ac8bca70e99
Jacques Chester <jchester@pivotal.io>
2017-04-18 11:53:21 -0400

    JUnitize AuthFailureAuditRecordDataServiceTest
    - Use HttpStatus for code
    
    [#143069351] Fix failing unit tests when running in parallel with gradle
    
    Signed-off-by: Ashwin Venkatesh <avenkatesh@pivotal.io>

--- src/test/java/io/pivotal/security/data/AuthFailureAuditRecordDataServiceTest.java
+++ src/test/java/io/pivotal/security/data/AuthFailureAuditRecordDataServiceTest.java
@@ -13,98 +13,116 @@ import static org.junit.Assert.assertNotNull;
 import com.greghaskins.spectrum.Spectrum;
 import io.pivotal.security.CredentialManagerApp;
 import io.pivotal.security.entity.AuthFailureAuditRecord;
+import io.pivotal.security.repository.AuthFailureAuditRecordRepository;
 import io.pivotal.security.util.CurrentTimeProvider;
 import io.pivotal.security.util.DatabaseProfileResolver;
 import java.time.Instant;
 import java.util.List;
+import javax.persistence.EntityManager;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(Spectrum.class)
+@RunWith(SpringRunner.class)
 @ActiveProfiles(value = {"unit-test"}, resolver = DatabaseProfileResolver.class)
-@SpringBootTest(classes = CredentialManagerApp.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 public class AuthFailureAuditRecordDataServiceTest {
 
   private final Instant frozenTime = Instant.ofEpochMilli(1400000000123L);
   private final long authValidFrom = frozenTime.toEpochMilli();
   private final long authValidUntil = authValidFrom + 10000;
-  @Autowired
-  AuthFailureAuditRecordDataService subject;
+
   @Autowired
   JdbcTemplate jdbcTemplate;
+
+  @Autowired
+  EntityManager entityManager;
+
+  @Autowired
+  AuthFailureAuditRecordRepository authFailureAuditRecordRepository;
+
   @MockBean
   CurrentTimeProvider currentTimeProvider;
 
-  {
-    wireAndUnwire(this);
+  AuthFailureAuditRecordDataService subject;
 
-    beforeEach(() -> {
-      mockOutCurrentTimeProvider(currentTimeProvider).accept(frozenTime.toEpochMilli());
-    });
+  @Before
+  public void beforeEach(){
+    mockOutCurrentTimeProvider(currentTimeProvider).accept(frozenTime.toEpochMilli());
 
-    describe("#save", () -> {
-      it("should create the entity in the database", () -> {
-        AuthFailureAuditRecord record = createAuthFailureAuditRecord();
-        record = subject.save(record);
+    subject = new AuthFailureAuditRecordDataService(authFailureAuditRecordRepository);
+  }
 
-        assertNotNull(record);
+  @Test
+  public void save_whenGivenARecord_savesTheRecord() {
+    AuthFailureAuditRecord record = createAuthFailureAuditRecord();
+    record = subject.save(record);
 
-        List<AuthFailureAuditRecord> records = jdbcTemplate
-            .query("select * from auth_failure_audit_record", (rs, rowCount) -> {
-              AuthFailureAuditRecord r = new AuthFailureAuditRecord();
-              r.setId(rs.getLong("id"));
-              r.setFailureDescription(rs.getString("failure_description"));
-              r.setHostName(rs.getString("host_name"));
-              r.setNow(Instant.ofEpochMilli(rs.getLong("now")));
-              r.setPath(rs.getString("path"));
-              r.setRequesterIp(rs.getString("requester_ip"));
-              r.setAuthValidUntil(rs.getLong("auth_valid_until"));
-              r.setAuthValidFrom(rs.getLong("auth_valid_from"));
-              r.setUaaUrl(rs.getString("uaa_url"));
-              r.setUserId(rs.getString("user_id"));
-              r.setUserName(rs.getString("user_name"));
-              r.setXForwardedFor(rs.getString("x_forwarded_for"));
-              r.setScope(rs.getString("scope"));
-              r.setGrantType(rs.getString("grant_type"));
-              r.setClientId(rs.getString("client_id"));
-              r.setMethod(rs.getString("method"));
-              r.setStatusCode(rs.getInt("status_code"));
-              r.setQueryParameters(rs.getString("query_parameters"));
-              return r;
-            });
+    entityManager.flush();
 
-        assertThat(records.size(), equalTo(1));
+    assertNotNull(record);
 
-        AuthFailureAuditRecord expected = records.get(0);
-        AuthFailureAuditRecord actual = record;
+    List<AuthFailureAuditRecord> records = jdbcTemplate
+        .query("select * from auth_failure_audit_record", (rs, rowCount) -> {
+          AuthFailureAuditRecord r = new AuthFailureAuditRecord();
+          r.setId(rs.getLong("id"));
+          r.setFailureDescription(rs.getString("failure_description"));
+          r.setHostName(rs.getString("host_name"));
+          r.setNow(Instant.ofEpochMilli(rs.getLong("now")));
+          r.setPath(rs.getString("path"));
+          r.setRequesterIp(rs.getString("requester_ip"));
+          r.setAuthValidUntil(rs.getLong("auth_valid_until"));
+          r.setAuthValidFrom(rs.getLong("auth_valid_from"));
+          r.setUaaUrl(rs.getString("uaa_url"));
+          r.setUserId(rs.getString("user_id"));
+          r.setUserName(rs.getString("user_name"));
+          r.setXForwardedFor(rs.getString("x_forwarded_for"));
+          r.setScope(rs.getString("scope"));
+          r.setGrantType(rs.getString("grant_type"));
+          r.setClientId(rs.getString("client_id"));
+          r.setMethod(rs.getString("method"));
+          r.setStatusCode(rs.getInt("status_code"));
+          r.setQueryParameters(rs.getString("query_parameters"));
+          return r;
+        });
 
-        assertThat(expected.getId(), equalTo(actual.getId()));
-        assertThat(expected.getFailureDescription(), equalTo(actual.getFailureDescription()));
-        assertThat(expected.getHostName(), equalTo(actual.getHostName()));
-        assertThat(expected.getNow(), equalTo(actual.getNow()));
-        assertThat(expected.getNow(), equalTo(frozenTime));
-        assertThat(expected.getPath(), equalTo(actual.getPath()));
-        assertThat(expected.getRequesterIp(), equalTo(actual.getRequesterIp()));
-        assertThat(expected.getAuthValidUntil(), equalTo(actual.getAuthValidUntil()));
-        assertThat(expected.getAuthValidUntil(), equalTo(authValidUntil));
-        assertThat(expected.getAuthValidFrom(), equalTo(actual.getAuthValidFrom()));
-        assertThat(expected.getAuthValidFrom(), equalTo(authValidFrom));
-        assertThat(expected.getUaaUrl(), equalTo(actual.getUaaUrl()));
-        assertThat(expected.getUserId(), equalTo(actual.getUserId()));
-        assertThat(expected.getUserName(), equalTo(actual.getUserName()));
-        assertThat(expected.getXForwardedFor(), equalTo(actual.getXForwardedFor()));
-        assertThat(expected.getScope(), equalTo(actual.getScope()));
-        assertThat(expected.getGrantType(), equalTo(actual.getGrantType()));
-        assertThat(expected.getClientId(), equalTo(actual.getClientId()));
-        assertThat(expected.getMethod(), equalTo(actual.getMethod()));
-        assertThat(expected.getStatusCode(), equalTo(actual.getStatusCode()));
-        assertThat(expected.getQueryParameters(), equalTo(actual.getQueryParameters()));
-      });
-    });
+    assertThat(records.size(), equalTo(1));
+
+    AuthFailureAuditRecord expected = records.get(0);
+    AuthFailureAuditRecord actual = record;
+
+    assertThat(expected.getId(), equalTo(actual.getId()));
+    assertThat(expected.getFailureDescription(), equalTo(actual.getFailureDescription()));
+    assertThat(expected.getHostName(), equalTo(actual.getHostName()));
+    assertThat(expected.getNow(), equalTo(actual.getNow()));
+    assertThat(expected.getNow(), equalTo(frozenTime));
+    assertThat(expected.getPath(), equalTo(actual.getPath()));
+    assertThat(expected.getRequesterIp(), equalTo(actual.getRequesterIp()));
+    assertThat(expected.getAuthValidUntil(), equalTo(actual.getAuthValidUntil()));
+    assertThat(expected.getAuthValidUntil(), equalTo(authValidUntil));
+    assertThat(expected.getAuthValidFrom(), equalTo(actual.getAuthValidFrom()));
+    assertThat(expected.getAuthValidFrom(), equalTo(authValidFrom));
+    assertThat(expected.getUaaUrl(), equalTo(actual.getUaaUrl()));
+    assertThat(expected.getUserId(), equalTo(actual.getUserId()));
+    assertThat(expected.getUserName(), equalTo(actual.getUserName()));
+    assertThat(expected.getXForwardedFor(), equalTo(actual.getXForwardedFor()));
+    assertThat(expected.getScope(), equalTo(actual.getScope()));
+    assertThat(expected.getGrantType(), equalTo(actual.getGrantType()));
+    assertThat(expected.getClientId(), equalTo(actual.getClientId()));
+    assertThat(expected.getMethod(), equalTo(actual.getMethod()));
+    assertThat(expected.getStatusCode(), equalTo(actual.getStatusCode()));
+    assertThat(expected.getQueryParameters(), equalTo(actual.getQueryParameters()));
   }
 
   AuthFailureAuditRecord createAuthFailureAuditRecord() {
@@ -125,7 +143,7 @@ public class AuthFailureAuditRecordDataServiceTest {
     record.setGrantType("test-grant-type");
     record.setClientId("test-client-id");
     record.setMethod("GET");
-    record.setStatusCode(500);
+    record.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
     record.setQueryParameters("query=param");
 
     return record;
