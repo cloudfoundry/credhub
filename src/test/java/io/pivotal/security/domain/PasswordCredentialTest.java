f45a07310ee28aff41afe185331607ee555493ea
Edie Beer <ebeer@pivotal.io>
2017-06-21 11:46:32 -0400

    [JUnitize Test] PasswordCredentialTest
    [#147469927] password generation parameters should be stored with snake case keys
    
    Signed-off-by: Ashwin Venkatesh <avenkatesh@pivotal.io>

--- src/test/java/io/pivotal/security/domain/PasswordCredentialTest.java
+++ src/test/java/io/pivotal/security/domain/PasswordCredentialTest.java
@@ -1,13 +1,21 @@
 package io.pivotal.security.domain;
 
-import static com.greghaskins.spectrum.Spectrum.beforeEach;
-import static com.greghaskins.spectrum.Spectrum.describe;
-import static com.greghaskins.spectrum.Spectrum.it;
-import static io.pivotal.security.helper.SpectrumHelper.itThrows;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.pivotal.security.entity.PasswordCredentialData;
+import io.pivotal.security.request.StringGenerationParameters;
+import io.pivotal.security.service.Encryption;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.UUID;
+
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsNull.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
@@ -15,24 +23,9 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.greghaskins.spectrum.Spectrum;
-import io.pivotal.security.entity.PasswordCredentialData;
-import io.pivotal.security.request.PermissionEntry;
-import io.pivotal.security.request.StringGenerationParameters;
-import io.pivotal.security.service.Encryption;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-import org.junit.runner.RunWith;
-
-@RunWith(Spectrum.class)
+@RunWith(JUnit4.class)
 public class PasswordCredentialTest {
 
-  private static final List<PermissionEntry> EMPTY_ENTRIES_LIST = new ArrayList<>();
-  private static final StringGenerationParameters NO_PASSWORD_PARAMS = null;
-  private static final PasswordCredential NO_EXISTING_NAMED_PASSWORD_CREDENTIAL = null;
-  private static final List<PermissionEntry> NULL_ENTRIES_LIST = null;
   private static final String PASSWORD = "my-password";
 
   private PasswordCredential subject;
@@ -46,107 +39,108 @@ public class PasswordCredentialTest {
   private byte[] encryptedParametersValue;
   private byte[] parametersNonce;
 
-  {
-    beforeEach(() -> {
-      canaryUuid = UUID.randomUUID();
-      encryptor = mock(Encryptor.class);
+  @Before
+  public void beforeEach() {
+    canaryUuid = UUID.randomUUID();
+    encryptor = mock(Encryptor.class);
 
-      encryptedValue = "fake-encrypted-value".getBytes();
-      nonce = "fake-nonce".getBytes();
-      encryptedParametersValue = "fake-encrypted-parameters".getBytes();
-      parametersNonce = "fake-parameters-nonce".getBytes();
+    encryptedValue = "fake-encrypted-value".getBytes();
+    nonce = "fake-nonce".getBytes();
+    encryptedParametersValue = "fake-encrypted-parameters".getBytes();
+    parametersNonce = "fake-parameters-nonce".getBytes();
 
-      generationParameters = new StringGenerationParameters()
-          .setExcludeLower(true)
-          .setLength(10);
-      String generationParametersJson = new ObjectMapper().writeValueAsString(generationParameters);
+    generationParameters = new StringGenerationParameters()
+        .setExcludeLower(true)
+        .setLength(10);
 
-      when(encryptor.encrypt(null))
-          .thenReturn(new Encryption(canaryUuid, null, null));
-      final Encryption encryption = new Encryption(canaryUuid, encryptedValue, nonce);
-      when(encryptor.encrypt(PASSWORD))
-          .thenReturn(encryption);
-      final Encryption parametersEncryption = new Encryption(canaryUuid, encryptedParametersValue, parametersNonce);
-      when(encryptor.encrypt(eq(generationParametersJson)))
-          .thenReturn(parametersEncryption);
+    String generationParametersJson = null;
+    try {
+      generationParametersJson = new ObjectMapper().writeValueAsString(generationParameters);
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+    }
 
-      when(encryptor.decrypt(encryption))
-          .thenReturn(PASSWORD);
-      when(encryptor.decrypt(parametersEncryption))
-          .thenReturn(generationParametersJson);
+    when(encryptor.encrypt(null))
+        .thenReturn(new Encryption(canaryUuid, null, null));
+    final Encryption encryption = new Encryption(canaryUuid, encryptedValue, nonce);
+    when(encryptor.encrypt(PASSWORD))
+        .thenReturn(encryption);
+    final Encryption parametersEncryption = new Encryption(canaryUuid, encryptedParametersValue, parametersNonce);
+    when(encryptor.encrypt(eq(generationParametersJson)))
+        .thenReturn(parametersEncryption);
 
-      passwordCredentialData = new PasswordCredentialData("/Foo");
-      subject = new PasswordCredential(passwordCredentialData);
-      subject.setEncryptor(encryptor);
-    });
+    when(encryptor.decrypt(encryption))
+        .thenReturn(PASSWORD);
+    when(encryptor.decrypt(parametersEncryption))
+        .thenReturn(generationParametersJson);
 
-    it("returns type password", () -> {
-      assertThat(subject.getCredentialType(), equalTo("password"));
-    });
+    passwordCredentialData = new PasswordCredentialData("/Foo");
+    subject = new PasswordCredential(passwordCredentialData);
+    subject.setEncryptor(encryptor);
+  }
 
-    describe("#getGenerationParameters", () -> {
-      beforeEach(() -> {
-        subject.setPasswordAndGenerationParameters(PASSWORD, null);
-        subject.getPassword();
-      });
+  @Test
+  public void getCredentialType_returnsPassword() {
+    assertThat(subject.getCredentialType(), equalTo("password"));
+  }
 
-      it("should call decrypt twice: once for password and once for parameters", () -> {
-        subject.getGenerationParameters();
+  @Test
+  public void getGenerationParameters_shouldCallDecryptTwice() {
+    subject.setPasswordAndGenerationParameters(PASSWORD, null);
 
-        verify(encryptor, times(2)).decrypt(any());
-      });
-    });
+    subject.getGenerationParameters();
 
-    describe("#getPassword", () -> {
-      beforeEach(() -> {
-        subject = new PasswordCredential("/Foo");
-        subject.setEncryptor(encryptor);
-        when(encryptor.encrypt(null))
-            .thenReturn(new Encryption(canaryUuid, null, null));
-        subject.setPasswordAndGenerationParameters(PASSWORD, null);
-        subject.getGenerationParameters();
-      });
+    verify(encryptor, times(2)).decrypt(any());
+  }
 
-      it("should call decrypt twice: once for password and once for parameters", () -> {
-        subject.getPassword();
+  @Test
+  public void getPassword_shouldCallDecryptOnce() {
+    subject = new PasswordCredential("/Foo");
+    subject.setEncryptor(encryptor);
+    when(encryptor.encrypt(null))
+        .thenReturn(new Encryption(canaryUuid, null, null));
+    subject.setPasswordAndGenerationParameters(PASSWORD, null);
 
-        verify(encryptor, times(2)).decrypt(any());
-      });
-    });
+    subject.getPassword();
 
-    describe("#setPasswordAndGenerationParameters", () -> {
-      it("sets the nonce and the encrypted value", () -> {
-        subject.setPasswordAndGenerationParameters(PASSWORD, null);
-        assertThat(passwordCredentialData.getEncryptedValue(), notNullValue());
-        assertThat(passwordCredentialData.getNonce(), notNullValue());
-      });
+    verify(encryptor, times(1)).decrypt(any());
+  }
 
-      it("can decrypt values", () -> {
-        subject.setPasswordAndGenerationParameters(PASSWORD, generationParameters);
+  @Test
+  public void setPasswordAndGenerationParameters_setsTheNonceAndEncryptedValue() {
+    subject.setPasswordAndGenerationParameters(PASSWORD, null);
+    assertThat(passwordCredentialData.getEncryptedValue(), notNullValue());
+    assertThat(passwordCredentialData.getNonce(), notNullValue());
+  }
 
-        assertThat(subject.getPassword(), equalTo(PASSWORD));
+  @Test
+  public void setPasswordAndGenerationParameters_canDecryptValues() {
+    subject.setPasswordAndGenerationParameters(PASSWORD, generationParameters);
 
-        assertThat(subject.getGenerationParameters().getLength(), equalTo(11));
-        assertThat(subject.getGenerationParameters().isExcludeLower(), equalTo(true));
-        assertThat(subject.getGenerationParameters().isExcludeUpper(), equalTo(false));
-      });
+    assertThat(subject.getPassword(), equalTo(PASSWORD));
 
-      itThrows("when setting a value that is null", IllegalArgumentException.class, () -> {
-        subject.setPasswordAndGenerationParameters(null, null);
-      });
+    assertThat(subject.getGenerationParameters().getLength(), equalTo(11));
+    assertThat(subject.getGenerationParameters().isExcludeLower(), equalTo(true));
+    assertThat(subject.getGenerationParameters().isExcludeUpper(), equalTo(false));
+  }
 
-      it("sets the parametersNonce and the encryptedGenerationParameters", () -> {
-        subject.setPasswordAndGenerationParameters(PASSWORD, generationParameters);
-        assertThat(passwordCredentialData.getEncryptedGenerationParameters(), notNullValue());
-        assertThat(passwordCredentialData.getParametersNonce(), notNullValue());
-      });
+  @Test(expected = IllegalArgumentException.class)
+  public void setPasswordAndGenerationParameters_throwsAnExceptionWhenSettingANullValue() {
+    subject.setPasswordAndGenerationParameters(null, null);
+  }
 
-      it("should set encrypted generation parameters and nonce to null if parameters are null",
-          () -> {
-            subject.setPasswordAndGenerationParameters(PASSWORD, null);
-            assertThat(passwordCredentialData.getEncryptedGenerationParameters(), nullValue());
-            assertThat(passwordCredentialData.getParametersNonce(), nullValue());
-          });
-    });
+  @Test
+  public void setPasswordAndGenerationParameters_setsParamsNonceAndEncryptedGenerationParameters() {
+    subject.setPasswordAndGenerationParameters(PASSWORD, generationParameters);
+
+    assertThat(passwordCredentialData.getEncryptedGenerationParameters(), notNullValue());
+    assertThat(passwordCredentialData.getParametersNonce(), notNullValue());
+  }
+
+  @Test
+  public void setPasswordAndGenerationParameters_shouldSetNullsIfTheParamsAreNulls() {
+    subject.setPasswordAndGenerationParameters(PASSWORD, null);
+    assertThat(passwordCredentialData.getEncryptedGenerationParameters(), nullValue());
+    assertThat(passwordCredentialData.getParametersNonce(), nullValue());
   }
 }
