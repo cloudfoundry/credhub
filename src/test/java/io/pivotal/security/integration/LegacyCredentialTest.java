86586112d94551f3af030afa47cc03ee1f20d4cb
Ashwin Venkatesh <avenkatesh@pivotal.io>
2017-06-22 12:13:00 -0400

    [Integration Test] Legacy Credentials with no ACLs
    [#142550283] Credentials that exist without any ACEs should bypass authorization enforcement
    
    Signed-off-by: Phil Goodwin <pgoodwin@pivotal.io>

---
+++ src/test/java/io/pivotal/security/integration/LegacyCredentialTest.java
@@ -0,0 +1,75 @@
+package io.pivotal.security.integration;
+
+import io.pivotal.security.CredentialManagerApp;
+import io.pivotal.security.data.CredentialDataService;
+import io.pivotal.security.domain.Encryptor;
+import io.pivotal.security.domain.ValueCredential;
+import io.pivotal.security.entity.ValueCredentialData;
+import io.pivotal.security.util.DatabaseProfileResolver;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+import static io.pivotal.security.util.AuthConstants.UAA_OAUTH2_CLIENT_CREDENTIALS_TOKEN;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = CredentialManagerApp.class)
+@ActiveProfiles(value = "unit-test", resolver = DatabaseProfileResolver.class)
+@Transactional
+@TestPropertySource(properties = "security.authorization.acls.enabled=true")
+public class LegacyCredentialTest {
+
+  @Autowired
+  WebApplicationContext webApplicationContext;
+
+  @Autowired
+  CredentialDataService credentialDataService;
+
+  @Autowired
+  Encryptor encryptor;
+
+  private MockMvc mockMvc;
+  private String CREDENTIAL_NAME;
+
+  @Before
+  public void setup() throws Exception {
+    CREDENTIAL_NAME = "/bob";
+    ValueCredentialData valueCredentialData = new ValueCredentialData(CREDENTIAL_NAME);
+    ValueCredential noAclsSecret = new ValueCredential(valueCredentialData);
+    noAclsSecret.setEncryptor(encryptor);
+    noAclsSecret.setValue("bob's value");
+
+    credentialDataService.save(noAclsSecret);
+    mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+        .apply(springSecurity())
+        .build();
+  }
+
+  @Test
+  public void GET_byCredentialName_whenTheCredentialHasNoAclsSet_returnsTheCredential()
+      throws Exception {
+    final MockHttpServletRequestBuilder get = get("/api/v1/data?name=" + CREDENTIAL_NAME)
+        .header("Authorization", "Bearer " + UAA_OAUTH2_CLIENT_CREDENTIALS_TOKEN);
+    mockMvc.perform(get)
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.data[0].type", equalTo("value")))
+        .andExpect(jsonPath("$.data[0].name", equalTo(CREDENTIAL_NAME)));
+  }
+}
