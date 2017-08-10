c05ea6fac1fcfa479b5bec3f7d9c565c924f8551
Kelly Gerritz <kgerritz@pivotal.io>
2017-08-10 10:01:47 -0700

    Remove unused imports
    [#149961918] pipeline server unit tests are failing for all database configurations
    
    Signed-off-by: Iryna Shustava <ishustava@pivotal.io>

--- src/test/java/io/pivotal/security/data/AuthFailureAuditRecordDataServiceTest.java
+++ src/test/java/io/pivotal/security/data/AuthFailureAuditRecordDataServiceTest.java
@@ -1,24 +1,9 @@
 package io.pivotal.security.data;
 
-import static com.greghaskins.spectrum.Spectrum.beforeEach;
-import static com.greghaskins.spectrum.Spectrum.describe;
-import static com.greghaskins.spectrum.Spectrum.it;
-import static io.pivotal.security.auth.UserContext.AUTH_METHOD_UAA;
-import static io.pivotal.security.helper.SpectrumHelper.mockOutCurrentTimeProvider;
-import static io.pivotal.security.helper.SpectrumHelper.wireAndUnwire;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.assertNotNull;
-
-import com.greghaskins.spectrum.Spectrum;
-import io.pivotal.security.CredentialManagerApp;
 import io.pivotal.security.entity.AuthFailureAuditRecord;
 import io.pivotal.security.repository.AuthFailureAuditRecordRepository;
 import io.pivotal.security.util.CurrentTimeProvider;
 import io.pivotal.security.util.DatabaseProfileResolver;
-import java.time.Instant;
-import java.util.List;
-import javax.persistence.EntityManager;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,12 +11,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.Instant;
+import java.util.List;
+import javax.persistence.EntityManager;
+
+import static io.pivotal.security.auth.UserContext.AUTH_METHOD_UAA;
+import static io.pivotal.security.helper.SpectrumHelper.mockOutCurrentTimeProvider;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @ActiveProfiles(value = {"unit-test"}, resolver = DatabaseProfileResolver.class)
@@ -59,7 +53,7 @@ public class AuthFailureAuditRecordDataServiceTest {
   private AuthFailureAuditRecord record;
 
   @Before
-  public void beforeEach(){
+  public void beforeEach() {
     mockOutCurrentTimeProvider(currentTimeProvider).accept(frozenTime.toEpochMilli());
 
     subject = new AuthFailureAuditRecordDataService(authFailureAuditRecordRepository);
