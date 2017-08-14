b9f87b815bd7ecce2e7e569de82793e233333ee0
Isobel Redelmeier <iredelmeier@pivotal.io>
2017-08-14 11:21:12 -0700

    JUnit-ize CredentialsControllerFindTest
    [#150233979] JUnit-ize Spectrum tests
    
    Signed-off-by: Ashwin Venkatesh <avenkatesh@pivotal.io>

--- src/test/java/io/pivotal/security/controller/v1/credential/CredentialsControllerFindTest.java
+++ src/test/java/io/pivotal/security/controller/v1/credential/CredentialsControllerFindTest.java
@@ -1,14 +1,38 @@
 package io.pivotal.security.controller.v1.credential;
 
-import static com.greghaskins.spectrum.Spectrum.beforeEach;
-import static com.greghaskins.spectrum.Spectrum.describe;
-import static com.greghaskins.spectrum.Spectrum.it;
-import static com.jayway.jsonassert.impl.matcher.IsCollectionWithSize.hasSize;
+import io.pivotal.security.CredentialManagerApp;
+import io.pivotal.security.data.CredentialDataService;
+import io.pivotal.security.helper.AuditingHelper;
+import io.pivotal.security.repository.EventAuditRecordRepository;
+import io.pivotal.security.repository.RequestAuditRecordRepository;
+import io.pivotal.security.util.CurrentTimeProvider;
+import io.pivotal.security.util.DatabaseProfileResolver;
+import io.pivotal.security.view.FindCredentialResult;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.function.Consumer;
+
 import static io.pivotal.security.audit.AuditingOperationCode.CREDENTIAL_FIND;
 import static io.pivotal.security.helper.SpectrumHelper.mockOutCurrentTimeProvider;
-import static io.pivotal.security.helper.SpectrumHelper.wireAndUnwire;
 import static io.pivotal.security.util.AuthConstants.UAA_OAUTH2_PASSWORD_GRANT_TOKEN;
 import static java.util.Collections.singletonList;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.doReturn;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
@@ -18,49 +42,26 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.greghaskins.spectrum.Spectrum;
-import io.pivotal.security.CredentialManagerApp;
-import io.pivotal.security.data.CredentialDataService;
-import io.pivotal.security.helper.AuditingHelper;
-import io.pivotal.security.repository.EventAuditRecordRepository;
-import io.pivotal.security.repository.RequestAuditRecordRepository;
-import io.pivotal.security.util.CurrentTimeProvider;
-import io.pivotal.security.util.DatabaseProfileResolver;
-import io.pivotal.security.view.FindCredentialResult;
-import java.time.Instant;
-import java.util.Arrays;
-import java.util.function.Consumer;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-@RunWith(Spectrum.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles(value = "unit-test", resolver = DatabaseProfileResolver.class)
 @SpringBootTest(classes = CredentialManagerApp.class)
+@Transactional
 public class CredentialsControllerFindTest {
 
   @Autowired
-  WebApplicationContext webApplicationContext;
+  private WebApplicationContext webApplicationContext;
 
   @SpyBean
-  CredentialDataService credentialDataService;
+  private CredentialDataService credentialDataService;
 
   @MockBean
-  CurrentTimeProvider mockCurrentTimeProvider;
+  private CurrentTimeProvider mockCurrentTimeProvider;
 
   @Autowired
-  RequestAuditRecordRepository requestAuditRecordRepository;
+  private RequestAuditRecordRepository requestAuditRecordRepository;
 
   @Autowired
-  EventAuditRecordRepository eventAuditRecordRepository;
+  private EventAuditRecordRepository eventAuditRecordRepository;
 
   private AuditingHelper auditingHelper;
 
@@ -68,146 +69,145 @@ public class CredentialsControllerFindTest {
 
   private Instant frozenTime = Instant.ofEpochSecond(1400011001L);
 
-  private Consumer<Long> fakeTimeSetter;
-
   private final String credentialName = "/my-namespace/subTree/credential-name";
 
-  private ResultActions response;
+  @Before
+  public void beforeEach() {
+    Consumer<Long> fakeTimeSetter = mockOutCurrentTimeProvider(mockCurrentTimeProvider);
 
-  {
-    wireAndUnwire(this);
+    fakeTimeSetter.accept(frozenTime.toEpochMilli());
+    mockMvc = MockMvcBuilders
+        .webAppContextSetup(webApplicationContext)
+        .apply(springSecurity())
+        .build();
 
-    beforeEach(() -> {
-      fakeTimeSetter = mockOutCurrentTimeProvider(mockCurrentTimeProvider);
+    auditingHelper = new AuditingHelper(requestAuditRecordRepository, eventAuditRecordRepository);
+  }
 
-      fakeTimeSetter.accept(frozenTime.toEpochMilli());
-      mockMvc = MockMvcBuilders
-          .webAppContextSetup(webApplicationContext)
-          .apply(springSecurity())
-          .build();
+  @Test
+  public void findCredentials_byNameLike_whenSearchTermContainsNoSlash_returnsCredentialMetadata() throws Exception {
+    ResultActions response = findCredentialsByNameLike();
 
-      auditingHelper = new AuditingHelper(requestAuditRecordRepository, eventAuditRecordRepository);
-    });
+    response.andExpect(status().isOk())
+        .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
+        .andExpect(jsonPath("$.credentials[0].name").value(credentialName))
+        .andExpect(jsonPath("$.credentials[0].version_created_at").value(frozenTime.toString()));
+  }
 
-    describe("finding credential", () -> {
-      describe("finding credentials by name-like, i.e. partial names, case-insensitively", () -> {
-        describe("when search term does not include a leading slash", () -> {
-          beforeEach(() -> {
-            String substring = credentialName.substring(4).toUpperCase();
-            doReturn(
-                singletonList(new FindCredentialResult(frozenTime, credentialName))
-            ).when(credentialDataService).findContainingName(substring);
-            final MockHttpServletRequestBuilder get = get("/api/v1/data?name-like=" + substring)
-                .header("Authorization", "Bearer " + UAA_OAUTH2_PASSWORD_GRANT_TOKEN)
-                .accept(APPLICATION_JSON);
+  @Test
+  public void findCredentials_byNameLike_whenSearchTermContainsNoSlash_persistsAnAuditEntry() throws Exception {
+    findCredentialsByNameLike();
 
-            this.response = mockMvc.perform(get);
-          });
+    auditingHelper.verifyAuditing(CREDENTIAL_FIND, null, "uaa-user:df0c1a26-2875-4bf5-baf9-716c6bb5ea6d", "/api/v1/data", 200);
+  }
 
-          it("should return the credential metadata", () -> {
-            this.response.andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
-                .andExpect(jsonPath("$.credentials[0].name").value(credentialName))
-                .andExpect(jsonPath("$.credentials[0].version_created_at").value(frozenTime.toString()));
-          });
+  @Test
+  public void findCredentials_byPath_returnsCredentialMetaData() throws Exception {
+    String substring = credentialName.substring(0, credentialName.lastIndexOf("/"));
+    doReturn(
+        singletonList(new FindCredentialResult(frozenTime, credentialName))
+    ).when(credentialDataService).findStartingWithPath(substring);
 
-          it("persists an audit entry", () -> {
-            auditingHelper.verifyAuditing(CREDENTIAL_FIND, null, "uaa-user:df0c1a26-2875-4bf5-baf9-716c6bb5ea6d", "/api/v1/data", 200);
-          });
-        });
-      });
+    final MockHttpServletRequestBuilder getResponse = get("/api/v1/data?path=" + substring)
+        .header("Authorization", "Bearer " + UAA_OAUTH2_PASSWORD_GRANT_TOKEN)
+        .accept(APPLICATION_JSON);
 
-      describe("finding credentials by path", () -> {
-        beforeEach(() -> {
-          String substring = credentialName.substring(0, credentialName.lastIndexOf("/"));
-          doReturn(
-              singletonList(new FindCredentialResult(frozenTime, credentialName))
-          ).when(credentialDataService).findStartingWithPath(substring);
+    mockMvc.perform(getResponse)
+        .andExpect(status().isOk())
+        .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
+        .andExpect(jsonPath("$.credentials[0].name").value(credentialName))
+        .andExpect(jsonPath("$.credentials[0].version_created_at").value(frozenTime.toString()));
+  }
 
-          final String path = substring;
-          final MockHttpServletRequestBuilder get = get("/api/v1/data?path=" + path)
-              .header("Authorization", "Bearer " + UAA_OAUTH2_PASSWORD_GRANT_TOKEN)
-              .accept(APPLICATION_JSON);
+  @Test
+  public void findCredentials_byPath_shouldOnlyFindPathsThatBeginWithSpecifiedSubstringCaseInsensitively() throws Exception {
+    final String path = "namespace";
 
-          this.response = mockMvc.perform(get);
-        });
+    assertTrue(credentialName.contains(path));
 
-        it("should return the credential metadata", () -> {
-          this.response.andExpect(status().isOk())
-              .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
-              .andExpect(jsonPath("$.credentials[0].name").value(credentialName))
-              .andExpect(jsonPath("$.credentials[0].version_created_at").value(frozenTime.toString()));
-        });
+    MockHttpServletRequestBuilder request = get("/api/v1/data?path=" + path.toUpperCase())
+        .header("Authorization", "Bearer " + UAA_OAUTH2_PASSWORD_GRANT_TOKEN)
+        .accept(APPLICATION_JSON);
 
-        it("should only find paths that start with the specified substring case-independently", () -> {
-          final String path = "namespace";
+    mockMvc.perform(request).andExpect(status().isOk())
+        .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
+        .andExpect(jsonPath("$.credentials", hasSize(0)));
+  }
 
-          assertTrue(credentialName.contains(path));
+  @Test
+  public void findCredentials_byPath_shouldReturnAllChildrenPrefixedWithThePathCaseInsensitively() throws Exception {
+    final String path = "/my-namespace";
+    doReturn(
+        singletonList(new FindCredentialResult(frozenTime, credentialName))
+    ).when(credentialDataService).findStartingWithPath(path.toUpperCase());
 
-          final MockHttpServletRequestBuilder get = get("/api/v1/data?path=" + path.toUpperCase())
-              .header("Authorization", "Bearer " + UAA_OAUTH2_PASSWORD_GRANT_TOKEN)
-              .accept(APPLICATION_JSON);
+    assertTrue(credentialName.startsWith(path));
 
-          mockMvc.perform(get).andExpect(status().isOk())
-              .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
-              .andExpect(jsonPath("$.credentials", hasSize(0)));
-        });
+    final MockHttpServletRequestBuilder getRequest = get("/api/v1/data?path=" + path.toUpperCase())
+        .header("Authorization", "Bearer " + UAA_OAUTH2_PASSWORD_GRANT_TOKEN)
+        .accept(APPLICATION_JSON);
 
-        it("should return all children which are prefixed with the path case-independently", () -> {
-          final String path = "/my-namespace";
-          doReturn(
-              singletonList(new FindCredentialResult(frozenTime, credentialName))
-          ).when(credentialDataService).findStartingWithPath(path.toUpperCase());
+    mockMvc.perform(getRequest).andExpect(status().isOk())
+        .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
+        .andExpect(jsonPath("$.credentials", hasSize(1)));
+  }
 
-          assertTrue(credentialName.startsWith(path));
+  @Test
+  public void findCredentials_byPath_shouldNotReturnCredentialsThatMatchThePathIncompletely() throws Exception {
+    final String path = "/my-namespace/subTr";
 
-          final MockHttpServletRequestBuilder get = get("/api/v1/data?path=" + path.toUpperCase())
-              .header("Authorization", "Bearer " + UAA_OAUTH2_PASSWORD_GRANT_TOKEN)
-              .accept(APPLICATION_JSON);
+    assertTrue(credentialName.startsWith(path));
 
-          mockMvc.perform(get).andExpect(status().isOk())
-              .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
-              .andExpect(jsonPath("$.credentials", hasSize(1)));
-        });
+    final MockHttpServletRequestBuilder get = get("/api/v1/data?path=" + path)
+        .header("Authorization", "Bearer " + UAA_OAUTH2_PASSWORD_GRANT_TOKEN)
+        .accept(APPLICATION_JSON);
 
-        it("should not findCredentialsUsingPath paths which start an existing path but matches incompletely", () -> {
-          final String path = "/my-namespace/subTr";
+    mockMvc.perform(get).andExpect(status().isOk())
+        .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
+        .andExpect(jsonPath("$.credentials", hasSize(0)));
+  }
 
-          assertTrue(credentialName.startsWith(path));
+  @Test
+  public void findCredentials_byPath_savesTheAuditLog() throws Exception {
+    String substring = credentialName.substring(0, credentialName.lastIndexOf("/"));
+    doReturn(
+        singletonList(new FindCredentialResult(frozenTime, credentialName))
+    ).when(credentialDataService).findStartingWithPath(substring);
 
-          final MockHttpServletRequestBuilder get = get("/api/v1/data?path=" + path)
-              .header("Authorization", "Bearer " + UAA_OAUTH2_PASSWORD_GRANT_TOKEN)
-              .accept(APPLICATION_JSON);
+    final MockHttpServletRequestBuilder request = get("/api/v1/data?path=" + substring)
+        .header("Authorization", "Bearer " + UAA_OAUTH2_PASSWORD_GRANT_TOKEN)
+        .accept(APPLICATION_JSON);
 
-          mockMvc.perform(get).andExpect(status().isOk())
-              .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
-              .andExpect(jsonPath("$.credentials", hasSize(0)));
-        });
+    mockMvc.perform(request);
 
-        it("persists an audit entry", () -> {
-          auditingHelper.verifyAuditing(CREDENTIAL_FIND, null, "uaa-user:df0c1a26-2875-4bf5-baf9-716c6bb5ea6d", "/api/v1/data", 200);
-        });
-      });
+    auditingHelper.verifyAuditing(CREDENTIAL_FIND, null, "uaa-user:df0c1a26-2875-4bf5-baf9-716c6bb5ea6d", "/api/v1/data", 200);
+  }
 
-      describe("finding all paths", () -> {
-        beforeEach(() -> {
-          final MockHttpServletRequestBuilder get = get("/api/v1/data?paths=true")
-              .header("Authorization", "Bearer " + UAA_OAUTH2_PASSWORD_GRANT_TOKEN)
-              .accept(APPLICATION_JSON);
-          doReturn(
-              Arrays.asList("my-namespace/", "my-namespace/subTree/")
-          ).when(credentialDataService).findAllPaths();
+  @Test
+  public void findCredentials_findingAllPaths_returnsAllPossibleCredentialsPaths() throws Exception {
+    final MockHttpServletRequestBuilder getRequest = get("/api/v1/data?paths=true")
+        .header("Authorization", "Bearer " + UAA_OAUTH2_PASSWORD_GRANT_TOKEN)
+        .accept(APPLICATION_JSON);
+    doReturn(
+        Arrays.asList("my-namespace/", "my-namespace/subTree/")
+    ).when(credentialDataService).findAllPaths();
 
-          this.response = mockMvc.perform(get);
-        });
+    mockMvc.perform(getRequest)
+        .andExpect(status().isOk())
+        .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
+        .andExpect(jsonPath("$.paths[0].path").value("my-namespace/"))
+        .andExpect(jsonPath("$.paths[1].path").value("my-namespace/subTree/"));
+  }
 
-        it("returns all possible paths for the table of existing credentials", () -> {
-          this.response.andExpect(status().isOk())
-              .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
-              .andExpect(jsonPath("$.paths[0].path").value("my-namespace/"))
-              .andExpect(jsonPath("$.paths[1].path").value("my-namespace/subTree/"));
-        });
-      });
-    });
+  private ResultActions findCredentialsByNameLike() throws Exception {
+    String substring = credentialName.substring(4).toUpperCase();
+    doReturn(
+        singletonList(new FindCredentialResult(frozenTime, credentialName))
+    ).when(credentialDataService).findContainingName(substring);
+    final MockHttpServletRequestBuilder get = get("/api/v1/data?name-like=" + substring)
+        .header("Authorization", "Bearer " + UAA_OAUTH2_PASSWORD_GRANT_TOKEN)
+        .accept(APPLICATION_JSON);
+
+    return mockMvc.perform(get);
   }
 }
