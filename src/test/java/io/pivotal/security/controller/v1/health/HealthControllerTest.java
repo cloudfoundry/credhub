787f40b96c30eeecac2eb619de679bbdbd6b93dd
Ashwin Venkatesh <avenkatesh@pivotal.io>
2017-08-14 11:25:24 -0700

    JUnit-ize HealthControllerTest
    [#150233979] JUnit-ize Spectrum tests
    
    Signed-off-by: Isobel Redelmeier <iredelmeier@pivotal.io>

--- src/test/java/io/pivotal/security/controller/v1/health/HealthControllerTest.java
+++ src/test/java/io/pivotal/security/controller/v1/health/HealthControllerTest.java
@@ -1,44 +1,43 @@
 package io.pivotal.security.controller.v1.health;
 
-import static com.greghaskins.spectrum.Spectrum.beforeEach;
-import static com.greghaskins.spectrum.Spectrum.it;
-import static io.pivotal.security.helper.SpectrumHelper.wireAndUnwire;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import com.greghaskins.spectrum.Spectrum;
 import io.pivotal.security.CredentialManagerApp;
 import io.pivotal.security.util.DatabaseProfileResolver;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.ConfigurableWebApplicationContext;
 
-@RunWith(Spectrum.class)
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles(value = "unit-test", resolver = DatabaseProfileResolver.class)
 @SpringBootTest(classes = CredentialManagerApp.class)
+@Transactional
 public class HealthControllerTest {
 
   @Autowired
-  protected ConfigurableWebApplicationContext context;
+  private ConfigurableWebApplicationContext context;
 
   private MockMvc mockMvc;
 
-  {
-    wireAndUnwire(this);
+  @Before
+  public void setUp() {
+    mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+  }
 
-    beforeEach(() -> {
-      mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-    });
-
-    it("can answer that we're healthy", () -> {
-      mockMvc.perform(get("/health"))
-          .andExpect(status().isOk())
-          .andExpect(content().json("{\"status\":\"UP\"}"));
-    });
+  @Test
+  public void healthEndpoint_respondsWhenCredHubIsHealthy() throws Exception {
+    mockMvc.perform(get("/health"))
+        .andExpect(status().isOk())
+        .andExpect(content().json("{\"status\":\"UP\"}"));
   }
 }
