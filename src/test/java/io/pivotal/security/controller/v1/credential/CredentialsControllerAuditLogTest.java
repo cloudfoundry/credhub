d27d74ebbf5373b81826fa8dd19fbe0682116d37
Ashwin Venkatesh <avenkatesh@pivotal.io>
2017-08-14 13:42:40 -0700

    JUnit-ize CredentialsControllerAuditLogTest
    [#150233979] JUnit-ize Spectrum tests
    
    Signed-off-by: Isobel Redelmeier <iredelmeier@pivotal.io>

--- src/test/java/io/pivotal/security/controller/v1/credential/CredentialsControllerAuditLogTest.java
+++ src/test/java/io/pivotal/security/controller/v1/credential/CredentialsControllerAuditLogTest.java
@@ -1,10 +1,9 @@
 package io.pivotal.security.controller.v1.credential;
 
-import com.greghaskins.spectrum.Spectrum;
 import io.pivotal.security.CredentialManagerApp;
-import io.pivotal.security.data.PermissionsDataService;
 import io.pivotal.security.data.CredentialDataService;
 import io.pivotal.security.data.EventAuditRecordDataService;
+import io.pivotal.security.data.PermissionsDataService;
 import io.pivotal.security.data.RequestAuditRecordDataService;
 import io.pivotal.security.domain.Credential;
 import io.pivotal.security.domain.Encryptor;
@@ -13,6 +12,8 @@ import io.pivotal.security.domain.ValueCredential;
 import io.pivotal.security.entity.EventAuditRecord;
 import io.pivotal.security.entity.RequestAuditRecord;
 import io.pivotal.security.util.DatabaseProfileResolver;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,23 +22,21 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
-import static com.greghaskins.spectrum.Spectrum.beforeEach;
-import static com.greghaskins.spectrum.Spectrum.describe;
-import static com.greghaskins.spectrum.Spectrum.it;
 import static io.pivotal.security.audit.AuditingOperationCode.CREDENTIAL_ACCESS;
 import static io.pivotal.security.audit.AuditingOperationCode.CREDENTIAL_DELETE;
 import static io.pivotal.security.audit.AuditingOperationCode.CREDENTIAL_UPDATE;
 import static io.pivotal.security.controller.v1.credential.CredentialsController.API_V1_DATA;
-import static io.pivotal.security.helper.SpectrumHelper.wireAndUnwire;
 import static io.pivotal.security.util.AuthConstants.UAA_OAUTH2_PASSWORD_GRANT_TOKEN;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -50,246 +49,120 @@ import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(Spectrum.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles(profiles = {"unit-test"}, resolver = DatabaseProfileResolver.class)
 @SpringBootTest(classes = CredentialManagerApp.class)
+@Transactional
 public class CredentialsControllerAuditLogTest {
 
   @Autowired
-  WebApplicationContext applicationContext;
+  private WebApplicationContext applicationContext;
 
   @SpyBean
-  EventAuditRecordDataService eventAuditRecordDataService;
+  private EventAuditRecordDataService eventAuditRecordDataService;
 
   @SpyBean
-  RequestAuditRecordDataService requestAuditRecordDataService;
+  private RequestAuditRecordDataService requestAuditRecordDataService;
 
   @MockBean
-  CredentialDataService credentialDataService;
+  private CredentialDataService credentialDataService;
 
   @MockBean
-  PermissionsDataService permissionsDataService;
+  private PermissionsDataService permissionsDataService;
 
   @SpyBean
-  Encryptor encryptor;
+  private Encryptor encryptor;
 
   private MockMvc mockMvc;
 
-  {
-    wireAndUnwire(this);
-
-    beforeEach(() -> {
-      mockMvc = MockMvcBuilders.webAppContextSetup(applicationContext)
-          .apply(springSecurity())
-          .build();
-    });
-
-    describe("when getting a credential", () -> {
-      describe("by name", () -> {
-        it("makes a credential_access audit log entry", () -> {
-          doReturn(Arrays.asList(new PasswordCredential("/foo").setEncryptor(encryptor)))
-              .when(credentialDataService).findAllByName(eq("foo"));
-
-          mockMvc.perform(get(API_V1_DATA + "?name=foo")
-              .accept(MediaType.APPLICATION_JSON)
-              .contentType(MediaType.APPLICATION_JSON)
-              .header("Authorization", "Bearer " + UAA_OAUTH2_PASSWORD_GRANT_TOKEN)
-              .header("X-Forwarded-For", "1.1.1.1,2.2.2.2"));
-
-          ArgumentCaptor<List> recordCaptor = ArgumentCaptor
-              .forClass(List.class);
-          verify(eventAuditRecordDataService, times(1)).save(recordCaptor.capture());
-
-          EventAuditRecord auditRecord = (EventAuditRecord) recordCaptor.getValue().get(0);
-
-          assertThat(auditRecord.getCredentialName(), equalTo("/foo"));
-          assertThat(auditRecord.getOperation(), equalTo(CREDENTIAL_ACCESS.toString()));
-        });
-      });
-
-      describe("by id", () -> {
-        it("makes a credential_access audit log entry", () -> {
-          doReturn(new PasswordCredential("/foo").setEncryptor(encryptor))
-              .when(credentialDataService).findByUuid(eq("foo-id"));
-
-          mockMvc.perform(get(API_V1_DATA + "/foo-id")
-              .accept(MediaType.APPLICATION_JSON)
-              .contentType(MediaType.APPLICATION_JSON)
-              .header("Authorization", "Bearer " + UAA_OAUTH2_PASSWORD_GRANT_TOKEN)
-              .header("X-Forwarded-For", "1.1.1.1,2.2.2.2"));
-
-          ArgumentCaptor<List> recordCaptor = ArgumentCaptor
-              .forClass(List.class);
-          verify(eventAuditRecordDataService, times(1)).save(recordCaptor.capture());
-
-          EventAuditRecord auditRecord = (EventAuditRecord) recordCaptor.getValue().get(0);
-
-          assertThat(auditRecord.getCredentialName(), equalTo("/foo"));
-          assertThat(auditRecord.getOperation(), equalTo(CREDENTIAL_ACCESS.toString()));
-        });
-      });
-    });
-
-    describe("when a request to set credential is served", () -> {
-      beforeEach(() -> {
-        when(credentialDataService.save(any(Credential.class))).thenAnswer(invocation -> {
-          ValueCredential valueCredential = invocation.getArgumentAt(0, ValueCredential.class);
-          valueCredential.setEncryptor(encryptor);
-          valueCredential.setUuid(UUID.randomUUID());
-          return valueCredential;
-        });
-
-        MockHttpServletRequestBuilder set = put(API_V1_DATA)
-            .accept(MediaType.APPLICATION_JSON)
-            .contentType(MediaType.APPLICATION_JSON)
-            .header("Authorization", "Bearer " + UAA_OAUTH2_PASSWORD_GRANT_TOKEN)
-            .header("X-Forwarded-For", "1.1.1.1,2.2.2.2")
-            .content("{\"type\":\"value\",\"name\":\"foo\",\"value\":\"credential\"}")
-            .with(request -> {
-              request.setRemoteAddr("12346");
-              return request;
-            });
-
-        mockMvc.perform(set)
-            .andExpect(status().isOk());
-      });
-
-      it("logs an audit record for credential update operation", () -> {
-        ArgumentCaptor<List> recordCaptor = ArgumentCaptor
-            .forClass(List.class);
-        verify(eventAuditRecordDataService, times(1)).save(recordCaptor.capture());
-
-        EventAuditRecord auditRecord = (EventAuditRecord) recordCaptor.getValue().get(0);
-
-        assertThat(auditRecord.getCredentialName(), equalTo("/foo"));
-        assertThat(auditRecord.getOperation(), equalTo(CREDENTIAL_UPDATE.toString()));
-      });
-    });
-
-    describe("when a request to generate a credential is served", () -> {
-      beforeEach(() -> {
-        when(credentialDataService.save(any(Credential.class))).thenAnswer(invocation -> {
-          PasswordCredential passwordCredential = invocation
-              .getArgumentAt(0, PasswordCredential.class);
-          passwordCredential.setUuid(UUID.randomUUID());
-          return passwordCredential;
-        });
-
-        MockHttpServletRequestBuilder post = post(API_V1_DATA)
-            .accept(MediaType.APPLICATION_JSON)
-            .contentType(MediaType.APPLICATION_JSON)
-            .header("Authorization", "Bearer " + UAA_OAUTH2_PASSWORD_GRANT_TOKEN)
-            .content("{\"type\":\"password\",\"name\":\"foo\"}")
-            .header("X-Forwarded-For", "1.1.1.1,2.2.2.2")
-            .with(request -> {
-              request.setRemoteAddr("12345");
-              return request;
-            });
-
-        mockMvc.perform(post)
-            .andExpect(status().isOk());
-      });
-
-      it("logs an audit record for credential_update operation", () -> {
-        ArgumentCaptor<List> recordCaptor = ArgumentCaptor
-            .forClass(List.class);
-        verify(eventAuditRecordDataService, times(1)).save(recordCaptor.capture());
-
-        EventAuditRecord auditRecord = (EventAuditRecord) recordCaptor.getValue().get(0);
-
-        assertThat(auditRecord.getCredentialName(), equalTo("/foo"));
-        assertThat(auditRecord.getOperation(), equalTo(CREDENTIAL_UPDATE.toString()));
-      });
-    });
-
-    describe("when a request to delete a credential is served", () -> {
-      beforeEach(() -> {
-        MockHttpServletRequestBuilder delete = delete(API_V1_DATA + "?name=foo")
-            .accept(MediaType.APPLICATION_JSON)
-            .contentType(MediaType.APPLICATION_JSON)
-            .header("Authorization", "Bearer " + UAA_OAUTH2_PASSWORD_GRANT_TOKEN)
-            .header("X-Forwarded-For", "1.1.1.1,2.2.2.2")
-            .with(request -> {
-              request.setRemoteAddr("12345");
-              return request;
-            });
-
-        mockMvc.perform(delete)
-            .andExpect(status().is4xxClientError());
-      });
-
-      it("logs an audit record for credential_delete operation", () -> {
-        ArgumentCaptor<List> recordCaptor = ArgumentCaptor
-            .forClass(List.class);
-        verify(eventAuditRecordDataService, times(1)).save(recordCaptor.capture());
-
-        EventAuditRecord auditRecord = (EventAuditRecord) recordCaptor.getValue().get(0);
-
-        assertThat(auditRecord.getCredentialName(), equalTo("/foo"));
-        assertThat(auditRecord.getOperation(), equalTo(CREDENTIAL_DELETE.toString()));
-      });
-    });
-
-    describe("when a request to retrieve a credential is served", () -> {
-      beforeEach(() -> {
-        doUnsuccessfulFetch("foo");
-      });
-
-      it("logs an audit record for credential_access operation", () -> {
-        ArgumentCaptor<List> recordCaptor = ArgumentCaptor
-            .forClass(List.class);
-        verify(eventAuditRecordDataService, times(1)).save(recordCaptor.capture());
-
-        EventAuditRecord auditRecord = (EventAuditRecord) recordCaptor.getValue().get(0);
-
-        assertThat(auditRecord.getOperation(), equalTo(CREDENTIAL_ACCESS.toString()));
-        assertThat(auditRecord.getCredentialName(), equalTo("/foo"));
-      });
-    });
-
-    describe("when a request has multiple X-Forwarded-For headers set", () -> {
-      beforeEach(() -> {
-        when(credentialDataService.save(any(Credential.class))).thenAnswer(invocation -> {
-          ValueCredential valueCredential = invocation.getArgumentAt(0, ValueCredential.class);
-          valueCredential.setUuid(UUID.randomUUID());
-          return valueCredential;
-        });
-
-        MockHttpServletRequestBuilder set = put(API_V1_DATA)
-            .accept(MediaType.APPLICATION_JSON)
-            .contentType(MediaType.APPLICATION_JSON)
-            .header("Authorization", "Bearer " + UAA_OAUTH2_PASSWORD_GRANT_TOKEN)
-            .header("X-Forwarded-For", "1.1.1.1,2.2.2.2")
-            .header("X-Forwarded-For", "3.3.3.3")
-            .content("{\"type\":\"value\",\"name\":\"foo\",\"value\":\"password\"}")
-            .with(request -> {
-              request.setRemoteAddr("12346");
-              return request;
-            });
-
-        mockMvc.perform(set)
-            .andExpect(status().isOk());
-      });
-
-      it("logs all X-Forwarded-For values", () -> {
-        ArgumentCaptor<RequestAuditRecord> recordCaptor = ArgumentCaptor
-            .forClass(RequestAuditRecord.class);
-
-        verify(requestAuditRecordDataService, times(1)).save(recordCaptor.capture());
-
-        RequestAuditRecord auditRecord = recordCaptor.getValue();
-
-        assertThat(auditRecord.getXForwardedFor(), equalTo("1.1.1.1,2.2.2.2,3.3.3.3"));
-      });
-    });
+  @Before
+  public void beforeEach() {
+    mockMvc = MockMvcBuilders.webAppContextSetup(applicationContext)
+        .apply(springSecurity())
+        .build();
   }
 
-  private void doUnsuccessfulFetch(String credentialName) throws Exception {
-    MockHttpServletRequestBuilder get = get(API_V1_DATA + "?name=" + credentialName)
+  @Test
+  public void gettingACredential_byName_makesACredentialAccessLogEntry() throws Exception {
+    doReturn(Arrays.asList(new PasswordCredential("/foo").setEncryptor(encryptor)))
+        .when(credentialDataService).findAllByName(eq("foo"));
+
+    mockMvc.perform(get(API_V1_DATA + "?name=foo")
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .header("Authorization", "Bearer " + UAA_OAUTH2_PASSWORD_GRANT_TOKEN)
+        .header("X-Forwarded-For", "1.1.1.1,2.2.2.2"));
+
+    ArgumentCaptor<List> recordCaptor = ArgumentCaptor
+        .forClass(List.class);
+    verify(eventAuditRecordDataService, times(1)).save(recordCaptor.capture());
+
+    EventAuditRecord auditRecord = (EventAuditRecord) recordCaptor.getValue().get(0);
+
+    assertThat(auditRecord.getCredentialName(), equalTo("/foo"));
+    assertThat(auditRecord.getOperation(), equalTo(CREDENTIAL_ACCESS.toString()));
+  }
+
+  @Test
+  public void gettingACredential_byId_makesACredentialAccessAuditLogEntry() throws Exception {
+    doReturn(new PasswordCredential("/foo").setEncryptor(encryptor))
+        .when(credentialDataService).findByUuid(eq("foo-id"));
+
+    mockMvc.perform(get(API_V1_DATA + "/foo-id")
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .header("Authorization", "Bearer " + UAA_OAUTH2_PASSWORD_GRANT_TOKEN)
+        .header("X-Forwarded-For", "1.1.1.1,2.2.2.2"));
+
+    ArgumentCaptor<List> recordCaptor = ArgumentCaptor
+        .forClass(List.class);
+    verify(eventAuditRecordDataService, times(1)).save(recordCaptor.capture());
+
+    EventAuditRecord auditRecord = (EventAuditRecord) recordCaptor.getValue().get(0);
+
+    assertThat(auditRecord.getCredentialName(), equalTo("/foo"));
+    assertThat(auditRecord.getOperation(), equalTo(CREDENTIAL_ACCESS.toString()));
+  }
+
+  @Test
+  public void settingACredential_makesACredentialUpdateLogEntry() throws Exception {
+    when(credentialDataService.save(any(Credential.class))).thenAnswer(invocation -> {
+      ValueCredential valueCredential = invocation.getArgumentAt(0, ValueCredential.class);
+      valueCredential.setEncryptor(encryptor);
+      valueCredential.setUuid(UUID.randomUUID());
+      return valueCredential;
+    });
+
+    MockHttpServletRequestBuilder set = put(API_V1_DATA)
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .header("Authorization", "Bearer " + UAA_OAUTH2_PASSWORD_GRANT_TOKEN)
+        .header("X-Forwarded-For", "1.1.1.1,2.2.2.2")
+        .content("{\"type\":\"value\",\"name\":\"foo\",\"value\":\"credential\"}")
+        .with(request -> {
+          request.setRemoteAddr("12346");
+          return request;
+        });
+
+    mockMvc.perform(set)
+        .andExpect(status().isOk());
+
+    ArgumentCaptor<List> recordCaptor = ArgumentCaptor
+        .forClass(List.class);
+    verify(eventAuditRecordDataService, times(1)).save(recordCaptor.capture());
+
+    EventAuditRecord auditRecord = (EventAuditRecord) recordCaptor.getValue().get(0);
+
+    assertThat(auditRecord.getCredentialName(), equalTo("/foo"));
+    assertThat(auditRecord.getOperation(), equalTo(CREDENTIAL_UPDATE.toString()));
+  }
+
+  @Test
+  public void deletingACredential_makesACredentialDeleteLogEntry() throws Exception {
+    MockHttpServletRequestBuilder deleteRequest = delete(API_V1_DATA + "?name=foo")
         .accept(MediaType.APPLICATION_JSON)
         .contentType(MediaType.APPLICATION_JSON)
         .header("Authorization", "Bearer " + UAA_OAUTH2_PASSWORD_GRANT_TOKEN)
@@ -299,7 +172,49 @@ public class CredentialsControllerAuditLogTest {
           return request;
         });
 
-    mockMvc.perform(get)
+    mockMvc.perform(deleteRequest)
         .andExpect(status().is4xxClientError());
+
+    ArgumentCaptor<List> recordCaptor = ArgumentCaptor
+        .forClass(List.class);
+    verify(eventAuditRecordDataService, times(1)).save(recordCaptor.capture());
+
+    EventAuditRecord auditRecord = (EventAuditRecord) recordCaptor.getValue().get(0);
+
+    assertThat(auditRecord.getCredentialName(), equalTo("/foo"));
+    assertThat(auditRecord.getOperation(), equalTo(CREDENTIAL_DELETE.toString()));
+  }
+
+  @Test
+  public void whenARequestHasMultipleXForwardedForHeaders_logsAllXForwardedForValues() throws Exception {
+    when(credentialDataService.save(any(Credential.class))).thenAnswer(invocation -> {
+      ValueCredential valueCredential = invocation.getArgumentAt(0, ValueCredential.class);
+      valueCredential.setUuid(UUID.randomUUID());
+      return valueCredential;
+    });
+
+    MockHttpServletRequestBuilder putRequest = put(API_V1_DATA)
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .header("Authorization", "Bearer " + UAA_OAUTH2_PASSWORD_GRANT_TOKEN)
+        .header("X-Forwarded-For", "1.1.1.1,2.2.2.2")
+        .header("X-Forwarded-For", "3.3.3.3")
+        .content("{\"type\":\"value\",\"name\":\"foo\",\"value\":\"password\"}")
+        .with(request -> {
+          request.setRemoteAddr("12346");
+          return request;
+        });
+
+    mockMvc.perform(putRequest)
+        .andExpect(status().isOk());
+
+    ArgumentCaptor<RequestAuditRecord> recordCaptor = ArgumentCaptor
+        .forClass(RequestAuditRecord.class);
+
+    verify(requestAuditRecordDataService, times(1)).save(recordCaptor.capture());
+
+    RequestAuditRecord auditRecord = recordCaptor.getValue();
+
+    assertThat(auditRecord.getXForwardedFor(), equalTo("1.1.1.1,2.2.2.2,3.3.3.3"));
   }
 }
