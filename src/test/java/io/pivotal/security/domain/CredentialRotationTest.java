d88612d734810109c8bfd3fd773067a6d4e420fc
Isobel Redelmeier <iredelmeier@pivotal.io>
2017-08-15 14:30:11 -0700

    JUnit-ize CredentialRotationTest
    [#150233979] JUnit-ize Spectrum tests
    
    Signed-off-by: Phil Goodwin <pgoodwin@pivotal.io>

--- src/test/java/io/pivotal/security/domain/CredentialRotationTest.java
+++ src/test/java/io/pivotal/security/domain/CredentialRotationTest.java
@@ -1,33 +1,31 @@
 package io.pivotal.security.domain;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.greghaskins.spectrum.Spectrum;
 import io.pivotal.security.entity.CertificateCredentialData;
+import io.pivotal.security.entity.CredentialData;
 import io.pivotal.security.entity.PasswordCredentialData;
 import io.pivotal.security.entity.RsaCredentialData;
-import io.pivotal.security.entity.CredentialData;
 import io.pivotal.security.entity.SshCredentialData;
 import io.pivotal.security.entity.ValueCredentialData;
 import io.pivotal.security.request.StringGenerationParameters;
 import io.pivotal.security.service.Encryption;
 import io.pivotal.security.service.EncryptionKeyCanaryMapper;
 import io.pivotal.security.service.RetryingEncryptionService;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import java.security.Key;
 import java.util.UUID;
 
-import static com.greghaskins.spectrum.Spectrum.beforeEach;
-import static com.greghaskins.spectrum.Spectrum.describe;
-import static com.greghaskins.spectrum.Spectrum.it;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@RunWith(Spectrum.class)
+@RunWith(JUnit4.class)
 public class CredentialRotationTest {
-
   private EncryptionKeyCanaryMapper encryptionKeyCanaryMapper;
   private Key activeEncryptionKey;
 
@@ -41,108 +39,98 @@ public class CredentialRotationTest {
   private RetryingEncryptionService encryptionService;
   private Encryptor encryptor;
 
-  {
-    describe("#rotate", () -> {
-      beforeEach(() -> {
-        encryptionKeyCanaryMapper = mock(EncryptionKeyCanaryMapper.class);
-        encryptionService = mock(RetryingEncryptionService.class);
-        encryptor = new Encryptor(encryptionService);
+  @Before
+  public void beforeEach() throws Exception {
+    encryptionKeyCanaryMapper = mock(EncryptionKeyCanaryMapper.class);
+    encryptionService = mock(RetryingEncryptionService.class);
+    encryptor = new Encryptor(encryptionService);
 
-        activeEncryptionKey = mock(Key.class);
-        oldEncryptionKey = mock(Key.class);
+    activeEncryptionKey = mock(Key.class);
+    oldEncryptionKey = mock(Key.class);
 
-        oldEncryptionKeyUuid = UUID.randomUUID();
-        activeEncryptionKeyUuid = UUID.randomUUID();
+    oldEncryptionKeyUuid = UUID.randomUUID();
+    activeEncryptionKeyUuid = UUID.randomUUID();
 
-        when(encryptionKeyCanaryMapper.getActiveUuid()).thenReturn(activeEncryptionKeyUuid);
-        when(encryptionKeyCanaryMapper.getActiveKey()).thenReturn(activeEncryptionKey);
-        when(encryptionKeyCanaryMapper.getKeyForUuid(activeEncryptionKeyUuid))
-            .thenReturn(activeEncryptionKey);
-        when(encryptionKeyCanaryMapper.getKeyForUuid(oldEncryptionKeyUuid))
-            .thenReturn(oldEncryptionKey);
+    when(encryptionKeyCanaryMapper.getActiveUuid()).thenReturn(activeEncryptionKeyUuid);
+    when(encryptionKeyCanaryMapper.getActiveKey()).thenReturn(activeEncryptionKey);
+    when(encryptionKeyCanaryMapper.getKeyForUuid(activeEncryptionKeyUuid))
+        .thenReturn(activeEncryptionKey);
+    when(encryptionKeyCanaryMapper.getKeyForUuid(oldEncryptionKeyUuid))
+        .thenReturn(oldEncryptionKey);
 
-        when(encryptionService.decrypt(new Encryption(oldEncryptionKeyUuid, "old-encrypted-value".getBytes(), "old-nonce".getBytes())))
-            .thenReturn("plaintext");
-        when(encryptionService.encrypt("plaintext"))
-            .thenReturn(new Encryption(activeEncryptionKeyUuid, "new-encrypted-value".getBytes(),
-                "new-nonce".getBytes()));
-      });
-
-      describe("when the credential contains an encrypted value", () -> {
-        describe("when the credential is a Certificate", () -> {
-          it("should re-encrypt with the active encryption key", () -> {
-            CertificateCredentialData certificateCredentialData =
-                new CertificateCredentialData("some-name");
-            CertificateCredential credential = new CertificateCredential(certificateCredentialData);
-            assertRotation(credential, certificateCredentialData);
-          });
-        });
-
-        describe("when the credential is a SSH key", () -> {
-          it("should re-encrypt with the active encryption key", () -> {
-            SshCredentialData sshCredentialData = new SshCredentialData("ssh-key");
-            SshCredential credential = new SshCredential(sshCredentialData);
-            assertRotation(credential, sshCredentialData);
-          });
-        });
-
-        describe("when the credential is a RSA key", () -> {
-          it("should re-encrypt with the active encryption key", () -> {
-            RsaCredentialData rsaCredentialData = new RsaCredentialData("rsa key");
-            RsaCredential credential = new RsaCredential(rsaCredentialData);
-            assertRotation(credential, rsaCredentialData);
-          });
-        });
-
-        describe("when the credential is a value credential", () -> {
-          it("should re-encrypt with the active encryption key", () -> {
-            ValueCredentialData valueCredentialData = new ValueCredentialData("value key");
-            ValueCredential credential = new ValueCredential(valueCredentialData);
-            assertRotation(credential, valueCredentialData);
-          });
-        });
-
-        describe("when the credential is a PasswordCredentialData", () -> {
-          it("should re-encrypt the password and the parameters with the active encryption key",
-              () -> {
-            PasswordCredentialData passwordCredentialData =
-                new PasswordCredentialData("some-name");
-            passwordCredentialData.setEncryptionKeyUuid(oldEncryptionKeyUuid);
-            passwordCredentialData.setEncryptedValue("old-encrypted-value".getBytes());
-            passwordCredentialData.setNonce("old-nonce".getBytes());
-            PasswordCredential password = new PasswordCredential(passwordCredentialData);
-            password.setEncryptor(encryptor);
-
-            passwordCredentialData.setEncryptedGenerationParameters("old-encrypted-parameters".getBytes());
-            passwordCredentialData.setParametersNonce("old-parameters-nonce".getBytes());
-
-            stringifiedParameters = new ObjectMapper()
-                .writeValueAsString(new StringGenerationParameters());
-
-            when(encryptionService
-                .decrypt(new Encryption(oldEncryptionKeyUuid, "old-encrypted-parameters".getBytes(), "old-parameters-nonce".getBytes())))
-                .thenReturn(stringifiedParameters);
-            when(encryptionService.encrypt(stringifiedParameters))
-                .thenReturn(new Encryption(activeEncryptionKeyUuid,
-                    "new-encrypted-parameters".getBytes(), "new-nonce-parameters".getBytes()));
-
-            password.rotate();
-            assertThat(passwordCredentialData.getEncryptionKeyUuid(),
-                equalTo(activeEncryptionKeyUuid));
-            assertThat(passwordCredentialData.getEncryptedValue(),
-                equalTo("new-encrypted-value".getBytes()));
-            assertThat(passwordCredentialData.getNonce(), equalTo("new-nonce".getBytes()));
-
-            assertThat(passwordCredentialData.getEncryptedGenerationParameters(),
-                equalTo("new-encrypted-parameters".getBytes()));
-            assertThat(passwordCredentialData.getParametersNonce(),
-                equalTo("new-nonce-parameters".getBytes()));
-          });
-        });
-      });
-    });
+    when(encryptionService.decrypt(new Encryption(oldEncryptionKeyUuid, "old-encrypted-value".getBytes(), "old-nonce".getBytes())))
+        .thenReturn("plaintext");
+    when(encryptionService.encrypt("plaintext"))
+        .thenReturn(new Encryption(activeEncryptionKeyUuid, "new-encrypted-value".getBytes(),
+            "new-nonce".getBytes()));
   }
 
+  @Test
+  public void rotate_givenCertificateCredential_reEncryptsWithActiveKey() {
+    CertificateCredentialData certificateCredentialData = new CertificateCredentialData("some-name");
+    CertificateCredential credential = new CertificateCredential(certificateCredentialData);
+
+    assertRotation(credential, certificateCredentialData);
+  }
+
+  @Test
+  public void rotate_givenSshCredential_reEncryptsWithActiveKey() {
+    SshCredentialData sshCredentialData = new SshCredentialData("ssh-key");
+    SshCredential credential = new SshCredential(sshCredentialData);
+
+    assertRotation(credential, sshCredentialData);
+  }
+
+  @Test
+  public void rotate_givenRsaCredential_reEncryptsWithActiveKey() {
+    RsaCredentialData rsaCredentialData = new RsaCredentialData("rsa key");
+    RsaCredential credential = new RsaCredential(rsaCredentialData);
+
+    assertRotation(credential, rsaCredentialData);
+  }
+
+  @Test
+  public void rotate_givenValueCredential_reEncryptsWithActiveKey() {
+    ValueCredentialData valueCredentialData = new ValueCredentialData("value key");
+    ValueCredential credential = new ValueCredential(valueCredentialData);
+
+    assertRotation(credential, valueCredentialData);
+  }
+
+  @Test
+  public void rotate_givenPasswordCredential_reEncryptsPasswordAndParametersWithActiveKey() throws Exception {
+    PasswordCredentialData passwordCredentialData = new PasswordCredentialData("some-name");
+    passwordCredentialData.setEncryptionKeyUuid(oldEncryptionKeyUuid);
+    passwordCredentialData.setEncryptedValue("old-encrypted-value".getBytes());
+    passwordCredentialData.setNonce("old-nonce".getBytes());
+    PasswordCredential password = new PasswordCredential(passwordCredentialData);
+    password.setEncryptor(encryptor);
+
+    passwordCredentialData.setEncryptedGenerationParameters("old-encrypted-parameters".getBytes());
+    passwordCredentialData.setParametersNonce("old-parameters-nonce".getBytes());
+
+    stringifiedParameters = new ObjectMapper()
+        .writeValueAsString(new StringGenerationParameters());
+
+    when(encryptionService
+        .decrypt(new Encryption(oldEncryptionKeyUuid, "old-encrypted-parameters".getBytes(), "old-parameters-nonce".getBytes())))
+        .thenReturn(stringifiedParameters);
+    when(encryptionService.encrypt(stringifiedParameters))
+        .thenReturn(new Encryption(activeEncryptionKeyUuid, "new-encrypted-parameters".getBytes(), "new-nonce-parameters".getBytes()));
+
+    password.rotate();
+
+    assertThat(passwordCredentialData.getEncryptionKeyUuid(),
+        equalTo(activeEncryptionKeyUuid));
+    assertThat(passwordCredentialData.getEncryptedValue(),
+        equalTo("new-encrypted-value".getBytes()));
+    assertThat(passwordCredentialData.getNonce(), equalTo("new-nonce".getBytes()));
+
+    assertThat(passwordCredentialData.getEncryptedGenerationParameters(),
+        equalTo("new-encrypted-parameters".getBytes()));
+    assertThat(passwordCredentialData.getParametersNonce(),
+        equalTo("new-nonce-parameters".getBytes()));
+  }
 
   private void assertRotation(Credential credential, CredentialData delegate) {
     credential.setEncryptor(encryptor);
