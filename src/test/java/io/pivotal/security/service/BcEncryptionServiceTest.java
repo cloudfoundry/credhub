4bca65f72ab7045a9ec463ad614875535ff874f7
Phil Goodwin <pgoodwin@pivotal.io>
2017-08-15 14:15:13 -0700

    JUnit-ize BcEncryptionServiceTest
    [#150233979] JUnit-ize Spectrum tests
    
    Signed-off-by: Isobel Redelmeier <iredelmeier@pivotal.io>

--- src/test/java/io/pivotal/security/service/BcEncryptionServiceTest.java
+++ src/test/java/io/pivotal/security/service/BcEncryptionServiceTest.java
@@ -1,27 +1,25 @@
 package io.pivotal.security.service;
 
-import com.greghaskins.spectrum.Spectrum;
 import io.pivotal.security.config.EncryptionKeyMetadata;
 import io.pivotal.security.util.PasswordKeyProxyFactoryTestImpl;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
-import static com.greghaskins.spectrum.Spectrum.it;
 import static io.pivotal.security.helper.SpectrumHelper.getBouncyCastleProvider;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-@RunWith(Spectrum.class)
+@RunWith(JUnit4.class)
 public class BcEncryptionServiceTest {
+  @Test
+  public void createsPasswordBasedKeyProxy() throws Exception {
+    BcEncryptionService subject = new BcEncryptionService(getBouncyCastleProvider(), new PasswordKeyProxyFactoryTestImpl());
 
-  {
-    it("should created a password-based key proxy", () -> {
-      BcEncryptionService subject = new BcEncryptionService(getBouncyCastleProvider(), new PasswordKeyProxyFactoryTestImpl());
+    EncryptionKeyMetadata keyMetadata = new EncryptionKeyMetadata();
+    keyMetadata.setEncryptionPassword("foobar");
 
-      EncryptionKeyMetadata keyMetadata = new EncryptionKeyMetadata();
-      keyMetadata.setEncryptionPassword("foobar");
-
-      final KeyProxy keyProxy = subject.createKeyProxy(keyMetadata);
-      assertThat(keyProxy, instanceOf(PasswordBasedKeyProxy.class));
-    });
+    final KeyProxy keyProxy = subject.createKeyProxy(keyMetadata);
+    assertThat(keyProxy, instanceOf(PasswordBasedKeyProxy.class));
   }
 }
