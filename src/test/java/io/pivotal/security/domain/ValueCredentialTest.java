6363d9c3493bdc664a0c21f4cc28fe980abaf431
Phil Goodwin <pgoodwin@pivotal.io>
2017-08-15 14:26:28 -0700

    JUnit-ize ValueCredentialTest
    [#150233979] JUnit-ize Spectrum tests
    
    Signed-off-by: Isobel Redelmeier <iredelmeier@pivotal.io>

--- src/test/java/io/pivotal/security/domain/ValueCredentialTest.java
+++ src/test/java/io/pivotal/security/domain/ValueCredentialTest.java
@@ -1,69 +1,74 @@
 package io.pivotal.security.domain;
 
-import com.greghaskins.spectrum.Spectrum;
 import io.pivotal.security.entity.ValueCredentialData;
 import io.pivotal.security.service.Encryption;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import java.util.UUID;
 
-import static com.greghaskins.spectrum.Spectrum.beforeEach;
-import static com.greghaskins.spectrum.Spectrum.describe;
-import static com.greghaskins.spectrum.Spectrum.it;
-import static io.pivotal.security.helper.SpectrumHelper.itThrows;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@RunWith(Spectrum.class)
+@RunWith(JUnit4.class)
 public class ValueCredentialTest {
 
-  ValueCredential subject;
+  private ValueCredential subject;
   private Encryptor encryptor;
   private UUID canaryUuid;
   private ValueCredentialData valueCredentialData;
 
-  {
-    beforeEach(() -> {
-      canaryUuid = UUID.randomUUID();
-      encryptor = mock(Encryptor.class);
-      byte[] encryptedValue = "fake-encrypted-value".getBytes();
-      byte[] nonce = "fake-nonce".getBytes();
-      final Encryption encryption = new Encryption(canaryUuid, encryptedValue, nonce);
-      when(encryptor.encrypt("my-value"))
-          .thenReturn(encryption);
-      when(encryptor.decrypt(encryption))
-          .thenReturn("my-value");
+  @Before
+  public void beforeEach() {
+    canaryUuid = UUID.randomUUID();
+    encryptor = mock(Encryptor.class);
+    byte[] encryptedValue = "fake-encrypted-value".getBytes();
+    byte[] nonce = "fake-nonce".getBytes();
+    final Encryption encryption = new Encryption(canaryUuid, encryptedValue, nonce);
+    when(encryptor.encrypt("my-value"))
+        .thenReturn(encryption);
+    when(encryptor.decrypt(encryption))
+        .thenReturn("my-value");
 
-      subject = new ValueCredential("Foo");
-    });
+    subject = new ValueCredential("Foo");
+  }
 
-    it("returns type value", () -> {
-      assertThat(subject.getCredentialType(), equalTo("value"));
-    });
+  @Test
+  public void getCredentialType_returnsCorrectType() {
+    assertThat(subject.getCredentialType(), equalTo("value"));
+  }
 
-    describe("with or without alternative names", () -> {
-      beforeEach(() -> {
-        valueCredentialData = new ValueCredentialData("foo");
-        subject = new ValueCredential(valueCredentialData).setEncryptor(encryptor);
-      });
+  @Test
+  public void setValue_encryptsValue() {
+    valueCredentialData = new ValueCredentialData("foo");
+    subject = new ValueCredential(valueCredentialData).setEncryptor(encryptor);
 
-      it("encrypts the value", () -> {
-        subject.setValue("my-value");
-        assertThat(valueCredentialData.getEncryptedValue(), notNullValue());
-        assertThat(valueCredentialData.getNonce(), notNullValue());
-      });
+    subject.setValue("my-value");
 
-      it("can decrypt values", () -> {
-        subject.setValue("my-value");
-        assertThat(subject.getValue(), equalTo("my-value"));
-      });
+    assertThat(valueCredentialData.getEncryptedValue(), notNullValue());
+    assertThat(valueCredentialData.getNonce(), notNullValue());
+  }
 
-      itThrows("when setting a value that is null", IllegalArgumentException.class, () -> {
-        subject.setValue(null);
-      });
-    });
+  @Test
+  public void getValue_decryptsValue() {
+    valueCredentialData = new ValueCredentialData("foo");
+    subject = new ValueCredential(valueCredentialData).setEncryptor(encryptor);
+
+    subject.setValue("my-value");
+
+    assertThat(subject.getValue(), equalTo("my-value"));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void setValue_whenValueIsNull_throwsException() {
+    valueCredentialData = new ValueCredentialData("foo");
+    subject = new ValueCredential(valueCredentialData).setEncryptor(encryptor);
+
+    subject.setValue(null);
   }
 }
