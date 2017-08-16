a64fcad94f519ae5345f498dd1451948f280af55
Kelly Gerritz <kgerritz@pivotal.io>
2017-08-16 15:45:32 -0700

    JUnit-ize SshViewTest
    [#150307774] Fix non-@Transactional tests
    
    Signed-off-by: Edie Beer <ebeer@pivotal.io>

--- src/test/java/io/pivotal/security/view/SshViewTest.java
+++ src/test/java/io/pivotal/security/view/SshViewTest.java
@@ -1,80 +1,75 @@
 package io.pivotal.security.view;
 
-import com.greghaskins.spectrum.Spectrum;
 import io.pivotal.security.domain.Encryptor;
 import io.pivotal.security.domain.SshCredential;
 import io.pivotal.security.helper.JsonTestHelper;
 import io.pivotal.security.service.Encryption;
 import io.pivotal.security.util.TestConstants;
 import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import java.time.Instant;
 import java.util.UUID;
 
-import static com.greghaskins.spectrum.Spectrum.beforeEach;
-import static com.greghaskins.spectrum.Spectrum.it;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@RunWith(Spectrum.class)
+@RunWith(JUnit4.class)
 public class SshViewTest {
+  private static final String CREDENTIAL_NAME = "/foo";
+  private static final UUID CREDENTIAL_UUID = java.util.UUID.randomUUID();
 
   private SshCredential entity;
 
-  private String credentialName;
+  @Before
+  public void beforeEach() {
+    Encryptor encryptor = mock(Encryptor.class);
+    final Encryption encryption = new Encryption(UUID.randomUUID(), "encrypted".getBytes(), "nonce".getBytes());
+    when(encryptor.encrypt(TestConstants.PRIVATE_KEY_4096)).thenReturn(
+        encryption);
+    when(encryptor.decrypt(encryption))
+        .thenReturn(TestConstants.PRIVATE_KEY_4096);
+    entity = new SshCredential(CREDENTIAL_NAME)
+        .setEncryptor(encryptor)
+        .setPublicKey(TestConstants.SSH_PUBLIC_KEY_4096_WITH_COMMENT)
+        .setPrivateKey(TestConstants.PRIVATE_KEY_4096);
+    entity.setUuid(CREDENTIAL_UUID);
+  }
 
-  private UUID uuid;
+  @Test
+  public void createsAViewFromEntity() {
+    final CredentialView subject = SshView.fromEntity(entity);
 
-  private Encryptor encryptor;
+    JSONObject obj = new JSONObject();
+    obj.put("public_key", TestConstants.SSH_PUBLIC_KEY_4096_WITH_COMMENT);
+    obj.put("private_key", TestConstants.PRIVATE_KEY_4096);
+    obj.put("public_key_fingerprint", "UmqxK9UJJR4Jrcw0DcwqJlCgkeQoKp8a+HY+0p0nOgc");
+    String expected = "{"
+        + "\"type\":\"ssh\","
+        + "\"version_created_at\":null,"
+        + "\"id\":\"" + CREDENTIAL_UUID.toString() + "\","
+        + "\"name\":\"/foo\","
+        + "\"value\":" + obj.toString() + "}";
+    String json = JsonTestHelper.serializeToString(subject);
+    assertThat(json, equalTo(expected));
+  }
 
-  {
-    beforeEach(() -> {
-      credentialName = "/foo";
-      uuid = UUID.randomUUID();
-      encryptor = mock(Encryptor.class);
-      final Encryption encryption = new Encryption(UUID.randomUUID(), "encrypted".getBytes(), "nonce".getBytes());
-      when(encryptor.encrypt(TestConstants.PRIVATE_KEY_4096)).thenReturn(
-          encryption);
-      when(encryptor.decrypt(encryption))
-          .thenReturn(TestConstants.PRIVATE_KEY_4096);
-      entity = new SshCredential(credentialName)
-          .setEncryptor(encryptor)
-          .setPublicKey(TestConstants.SSH_PUBLIC_KEY_4096_WITH_COMMENT)
-          .setPrivateKey(TestConstants.PRIVATE_KEY_4096);
-      entity.setUuid(uuid);
-    });
+  @Test
+  public void setsUpdatedAtTimeOnGeneratedView() {
+    Instant now = Instant.now();
+    entity.setVersionCreatedAt(now);
+    final SshView subject = (SshView) SshView.fromEntity(entity);
+    assertThat(subject.getVersionCreatedAt(), equalTo(now));
+  }
 
-    it("creates a view from entity", () -> {
-      final CredentialView subject = SshView.fromEntity(entity);
-
-      JSONObject obj = new JSONObject();
-      obj.put("public_key", TestConstants.SSH_PUBLIC_KEY_4096_WITH_COMMENT);
-      obj.put("private_key", TestConstants.PRIVATE_KEY_4096);
-      obj.put("public_key_fingerprint", "UmqxK9UJJR4Jrcw0DcwqJlCgkeQoKp8a+HY+0p0nOgc");
-      String expected = "{"
-          + "\"type\":\"ssh\","
-          + "\"version_created_at\":null,"
-          + "\"id\":\"" + uuid.toString() + "\","
-          + "\"name\":\"/foo\","
-          + "\"value\":"
-          + obj.toString() + "}";
-      String json = JsonTestHelper.serializeToString(subject);
-      assertThat(json, equalTo(expected));
-    });
-
-    it("sets updated-at time on generated view", () -> {
-      Instant now = Instant.now();
-      entity.setVersionCreatedAt(now);
-      final SshView subject = (SshView) SshView.fromEntity(entity);
-      assertThat(subject.getVersionCreatedAt(), equalTo(now));
-    });
-
-    it("sets uuid on generated view", () -> {
-      SshView subject = (SshView) SshView.fromEntity(entity);
-      assertThat(subject.getUuid(), equalTo(uuid.toString()));
-    });
+  @Test
+  public void setsUuidOnGeneratedView() {
+    SshView subject = (SshView) SshView.fromEntity(entity);
+    assertThat(subject.getUuid(), equalTo(CREDENTIAL_UUID.toString()));
   }
 }
