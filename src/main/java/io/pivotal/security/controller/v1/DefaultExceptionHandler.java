27634383c0243b8ef9ba37fc6ec69cce24cd789d
Iryna Shustava <ishustava@pivotal.io>
2017-09-19 21:14:26 -0400

    DefaultExceptionHandler returns consisten error response
    [#150938664] Non-handled exceptions shouldn't leak information

--- src/main/java/io/pivotal/security/controller/v1/DefaultExceptionHandler.java
+++ src/main/java/io/pivotal/security/controller/v1/DefaultExceptionHandler.java
@@ -1,36 +1,27 @@
 package io.pivotal.security.controller.v1;
 
-import org.springframework.boot.autoconfigure.web.DefaultErrorAttributes;
+import io.pivotal.security.view.ResponseError;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.Map;
-import javax.servlet.http.HttpServletRequest;
-
-@ControllerAdvice
+@RestControllerAdvice
 public class DefaultExceptionHandler {
+
+  private final MessageSourceAccessor messageSourceAccessor;
+
+  @Autowired
+  DefaultExceptionHandler(MessageSourceAccessor messageSourceAccessor) {
+    this.messageSourceAccessor = messageSourceAccessor;
+  }
+
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   @ExceptionHandler(Exception.class)
-  @ResponseBody
-  public Map<String, Object> handleGeneralException(HttpServletRequest request)
+  public ResponseError handleGeneralException()
       throws Exception {
-    RequestAttributes requestAttributes = new ServletRequestAttributes(request);
-
-    final Map<String, Object> errorAttributes = new DefaultErrorAttributes()
-        .getErrorAttributes(requestAttributes, false);
-    int status = (int) errorAttributes.getOrDefault("status", 500);
-
-    if (status == 999) {
-      status = 500;
-    }
-
-    errorAttributes.put("status", status);
-
-    return errorAttributes;
+    return new ResponseError(messageSourceAccessor.getMessage("error.internal_server_error"));
   }
 }
