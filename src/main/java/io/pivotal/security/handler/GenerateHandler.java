9b549a2f699c1ccbb7d19e420fef72eec5944a00
Phil Goodwin <pgoodwin@pivotal.io>
2017-09-22 16:13:09 -0700

    Extracted UniversalCredentialGenerator
    It can generate anything!
    Pulled out of GenerateHandler.
    
    [#149855615] Generating a certificate should fail if user doesn't have read operation for signing CA

--- src/main/java/io/pivotal/security/handler/GenerateHandler.java
+++ src/main/java/io/pivotal/security/handler/GenerateHandler.java
@@ -4,7 +4,6 @@ import io.pivotal.security.audit.EventAuditRecordParameters;
 import io.pivotal.security.auth.UserContext;
 import io.pivotal.security.credential.CredentialValue;
 import io.pivotal.security.generator.CertificateGenerator;
-import io.pivotal.security.generator.CredentialGenerator;
 import io.pivotal.security.generator.PasswordCredentialGenerator;
 import io.pivotal.security.generator.RsaGenerator;
 import io.pivotal.security.generator.SshGenerator;
@@ -19,77 +18,51 @@ import io.pivotal.security.view.CredentialView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class GenerateHandler {
 
-  private PasswordCredentialGenerator passwordCredentialGenerator;
-  private UserGenerator userGenerator;
-  private SshGenerator sshGenerator;
-  private RsaGenerator rsaGenerator;
-  private CertificateGenerator certificateGenerator;
   private final CredentialService credentialService;
-  private Map<String, CredentialGenerator> credentialGenerators;
+  private final UniversalCredentialGenerator credentialGenerator;
 
   @Autowired
   public GenerateHandler(
       CredentialService credentialService,
-      PasswordCredentialGenerator passwordCredentialGenerator,
-      UserGenerator userGenerator, SshGenerator sshGenerator,
-      RsaGenerator rsaGenerator,
-      CertificateGenerator certificateGenerator) {
+      UniversalCredentialGenerator credentialGenerator) {
     this.credentialService = credentialService;
-    this.passwordCredentialGenerator = passwordCredentialGenerator;
-    this.userGenerator = userGenerator;
-    this.sshGenerator = sshGenerator;
-    this.rsaGenerator = rsaGenerator;
-    this.certificateGenerator = certificateGenerator;
-
-    this.credentialGenerators = new HashMap<>();
-    this.credentialGenerators.put("password", this.passwordCredentialGenerator);
-    this.credentialGenerators.put("user", this.userGenerator);
-    this.credentialGenerators.put("ssh", this.sshGenerator);
-    this.credentialGenerators.put("rsa", this.rsaGenerator);
-    this.credentialGenerators.put("certificate", this.certificateGenerator);
+    this.credentialGenerator = credentialGenerator;
   }
 
   public CredentialView handle(
-      BaseCredentialGenerateRequest requestBody,
+      BaseCredentialGenerateRequest generateRequest,
       UserContext userContext,
       PermissionEntry currentUserPermissionEntry,
       List<EventAuditRecordParameters> auditRecordParameters
   ) {
 
-    CredentialValue value = generateCredential(requestBody, userContext);
+    CredentialValue value = credentialGenerator.generate(generateRequest, userContext);
 
     StringGenerationParameters generationParameters = null;
-    if (requestBody instanceof PasswordGenerateRequest) {
-      generationParameters = ((PasswordGenerateRequest) requestBody).getGenerationParameters();
+    if (generateRequest instanceof PasswordGenerateRequest) {
+      generationParameters = ((PasswordGenerateRequest) generateRequest).getGenerationParameters();
     }
 
-    if (requestBody instanceof UserGenerateRequest) {
-      generationParameters = ((UserGenerateRequest) requestBody)
+    if (generateRequest instanceof UserGenerateRequest) {
+      generationParameters = ((UserGenerateRequest) generateRequest)
           .getUserCredentialGenerationParameters();
     }
 
     return credentialService.save(
-        requestBody.getName(),
-        requestBody.getType(),
+        generateRequest.getName(),
+        generateRequest.getType(),
         value,
         generationParameters,
-        requestBody.getAdditionalPermissions(),
-        requestBody.isOverwrite(),
+        generateRequest.getAdditionalPermissions(),
+        generateRequest.isOverwrite(),
         userContext,
         currentUserPermissionEntry,
         auditRecordParameters
     );
-  }
-
-  private CredentialValue generateCredential(BaseCredentialGenerateRequest requestBody, UserContext userContext) {
-    CredentialGenerator generator = credentialGenerators.get(requestBody.getType());
-    return generator.generateCredential(requestBody.getDomainGenerationParameters(), userContext);
   }
 }
