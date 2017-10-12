480264759f50fea620c2eb30284e33d1855e0bdb
Ashwin Venkatesh <avenkatesh@pivotal.io>
2017-10-12 17:43:04 -0400

    Populate latest release in ENV
    [#151772041] Improve default version of credhub dev server

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+curl -v --silent "http://bosh.io/releases/github.com/pivotal-cf/credhub-release?all=1" | grep --line-buffered "Upload latest version, currently" | grep -o "[0-9]\.[0-9]\.[0-9]" > version
+export VERSION=$(cat version)
+
 ./setup_dev_mtls.sh
 ./gradlew --no-daemon bootRun $@
