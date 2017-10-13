eddb5f1db2f211b3a600b26da11a4bb15de43f60
Ashwin Venkatesh <avenkatesh@pivotal.io>
2017-10-13 09:09:38 -0400

    Use static version when running server in dev mode
    [#151772041] Improve default version of credhub dev server

--- start_server.sh
+++ start_server.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-curl -v --silent "http://bosh.io/releases/github.com/pivotal-cf/credhub-release?all=1" | grep --line-buffered "Upload latest version, currently" | grep -o "[0-9]\.[0-9]\.[0-9]" > version
-export VERSION=$(cat version)
+export VERSION=100.0.1
 
 ./setup_dev_mtls.sh
 ./gradlew --no-daemon bootRun $@
