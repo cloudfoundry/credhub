8337485b98cf6bf341727557fe52dfe219a9609e
Chhavi Kankaria <ckankaria@pivotal.io>
2018-03-28 14:34:00 -0400

    the max key length is 767 bytes for our version of mysql, updated the varchar in the migration script accordingly
    [/#156311992] CredHub allows credential names up to 1024 characters log
    
    Signed-off-by: Guillermo Kardolus <gkardolus@pivotal.io>

--- src/main/resources/db/migration/mysql/V47__increase_length_credential_name_column.sql
+++ src/main/resources/db/migration/mysql/V47__increase_length_credential_name_column.sql
@@ -1,8 +1,8 @@
 ALTER TABLE credential
-  MODIFY COLUMN name VARCHAR(767) NOT NULL;
+  MODIFY COLUMN name VARCHAR(255) NOT NULL;
 
 ALTER TABLE certificate_credential
-  MODIFY COLUMN ca_name VARCHAR(767);
+  MODIFY COLUMN ca_name VARCHAR(255);
 
 ALTER TABLE event_audit_record
-  MODIFY COLUMN credential_name VARCHAR(767);
+  MODIFY COLUMN credential_name VARCHAR(255);
