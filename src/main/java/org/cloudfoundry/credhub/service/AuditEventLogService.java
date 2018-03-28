ad60b65cb870e343f4e4ff45dc9c42792c7a3a52
Chhavi Kankaria <ckankaria@pivotal.io>
2018-03-28 13:50:00 -0400

    removed TODO comment
    [#155989634] CredHub Service Broker can be deployed via BOSH release
    
    Signed-off-by: Guillermo Kardolus <gkardolus@pivotal.io>

--- src/main/java/org/cloudfoundry/credhub/service/AuditEventLogService.java
+++ src/main/java/org/cloudfoundry/credhub/service/AuditEventLogService.java
@@ -3,7 +3,6 @@ package org.cloudfoundry.credhub.service;
 import org.apache.logging.log4j.Logger;
 import org.cloudfoundry.credhub.config.VersionProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 
 public class AuditEventLogService {
 
@@ -14,12 +13,5 @@ public class AuditEventLogService {
   public AuditEventLogService(VersionProvider versionProvider, Logger securityEventsLogger){
     this.credhubVersion = versionProvider.currentVersion();
     this.securityEventsLogger = securityEventsLogger;
-  }
-
-  public void log(HttpRequest request){ // TODO what do we pass here?
-//    CEFAuditRecord auditRecord = new CEFAuditRecord();
-//    auditRecord.setCredhubServerVersion(this.credhubVersion);
-//
-//    securityEventsLogger.info(auditRecord);
   }
 }
