33b0904871a7978556e0a4ced0452c0bcae0bcfa
Pivotal <pivotal@poppenhusen.nyc.pivotal.io>
2018-03-28 16:51:11 -0400

    Revert "migrated the credential table to use SHA instead of the name as unique"
    This reverts commit e67863824cb24525dae130241a9d2f9475c4e5af.

--- src/main/resources/db/migration/mysql/V47__increase_length_credential_name_column.sql
+++ src/main/resources/db/migration/mysql/V47__increase_length_credential_name_column.sql
@@ -1,25 +1,8 @@
-CREATE TABLE credential_new LIKE credential;
-
-ALTER TABLE credential_new ADD name_sha1 CHAR(40);
-
-ALTER TABLE credential_new ADD UNIQUE KEY (name_sha1);
-
-INSERT INTO credential_new (uuid,name,name_sha1)
-  SELECT uuid,name,SHA1(name) FROM credential;
-
-DROP INDEX name_unique ON credential_new;
-
-ALTER TABLE credential RENAME credential_old;
-
-ALTER TABLE credential_new RENAME credential;
-
-DROP TABLE credential_old;
-
 ALTER TABLE credential
   MODIFY COLUMN name VARCHAR(255) NOT NULL;
 
 ALTER TABLE certificate_credential
-  MODIFY COLUMN ca_name VARCHAR(1024);
+  MODIFY COLUMN ca_name VARCHAR(255);
 
 ALTER TABLE event_audit_record
-  MODIFY COLUMN credential_name VARCHAR(1024);
+  MODIFY COLUMN credential_name VARCHAR(255);
