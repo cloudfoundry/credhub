dce58f1ac3eefeeedf03864324a9f5f2a1d51ad7
Guillermo Kardolus <gkardolus@pivotal.io>
2018-03-28 16:03:27 -0400

    Revert "Created database migration scripts and created an integration test"
    This reverts commit 5ded800b9b04a99cc61cced2f95cace0152755ef.
    
    Signed-off-by: Ben Moss <bmoss@pivotal.io>

--- src/main/resources/db/migration/h2/V47__increase_length_credential_name_column.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE credential
-  ALTER COLUMN name VARCHAR(1024) DEFAULT NOT NULL;
-
-ALTER TABLE certificate_credential
-  ALTER COLUMN ca_name VARCHAR(1024);
-
-ALTER TABLE event_audit_record
-  ALTER COLUMN credential_name VARCHAR(1024);
