97b5a9e47928390e79826482ddd168b5de7e7000
Pivotal <pivotal@poppenhusen.nyc.pivotal.io>
2018-03-28 16:51:21 -0400

    Revert "Revert "Revert "Created database migration scripts and created an integration test"""
    This reverts commit f96065005128c6221aa41fa7d9b8ab2930b44c65.

--- src/main/resources/db/migration/mysql/V47__increase_length_credential_name_column.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE credential
-  MODIFY COLUMN name VARCHAR(255) NOT NULL;
-
-ALTER TABLE certificate_credential
-  MODIFY COLUMN ca_name VARCHAR(255);
-
-ALTER TABLE event_audit_record
-  MODIFY COLUMN credential_name VARCHAR(255);
