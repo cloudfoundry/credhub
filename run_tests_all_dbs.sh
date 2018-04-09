81eeb4068453dbf3efcf62f4f9da3be43397d9d1
Guillermo Kardolus <gkardolus@pivotal.io>
2018-04-09 10:19:59 -0400

    Only dropping databases if they exist
    [#156509406] Write a script to run tests on all 3 DBs
    
    Signed-off-by: Guillermo Kardolus <gkardolus@pivotal.io>

--- run_tests_all_dbs.sh
+++ run_tests_all_dbs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -ex
 
-echo '******* RUNNING ALL TESTS WITH H2 *******'
+printf "******* RUNNING ALL TESTS WITH H2 *******\n"
 ./gradlew --no-daemon clean test
 
-echo '******* RUNNING ALL TESTS WITH MYSQL *******'
-mysql --user=root --protocol=tcp --execute='DROP DATABASE credhub_test; CREATE DATABASE credhub_test'
+printf "******* RUNNING ALL TESTS WITH MYSQL *******\n"
+mysql --user=root --protocol=tcp --execute='DROP DATABASE IF EXISTS credhub_test; CREATE DATABASE credhub_test'
 ./gradlew --no-daemon clean test --info -Dspring.profiles.active=unit-test-mysql
 
-echo '******* RUNNING ALL TESTS WITH POSTGRES *******'
-psql -U pivotal -c "DROP DATABASE credhub_test" -c "CREATE DATABASE credhub_test"
+printf "******* RUNNING ALL TESTS WITH POSTGRES *******\n"
+psql -U pivotal -c "DROP DATABASE IF EXISTS credhub_test" -c "CREATE DATABASE credhub_test"
 ./gradlew --no-daemon clean test --info -Dspring.profiles.active=unit-test-postgres
