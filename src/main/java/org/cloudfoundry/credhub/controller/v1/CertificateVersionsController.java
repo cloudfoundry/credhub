ada27d36ef82efb01dfcf86f1d7bf4c0e30e28b2
Ben Moss <bmoss@pivotal.io>
2018-04-17 09:35:21 -0400

    Remove auditing from CertificateVersionsController
    Signed-off-by: Guillermo Kardolus <gkardolus@pivotal.io>

--- src/main/java/org/cloudfoundry/credhub/controller/v1/CertificateVersionsController.java
+++ src/main/java/org/cloudfoundry/credhub/controller/v1/CertificateVersionsController.java
@@ -1,6 +1,5 @@
 package org.cloudfoundry.credhub.controller.v1;
 
-import org.cloudfoundry.credhub.audit.EventAuditLogService;
 import org.cloudfoundry.credhub.handler.CertificatesHandler;
 import org.cloudfoundry.credhub.request.CreateVersionRequest;
 import org.cloudfoundry.credhub.view.CertificateView;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.util.List;
 
 import static org.cloudfoundry.credhub.controller.v1.CertificateVersionsController.API_V1_CERTIFICATE_VERSIONS;
@@ -29,43 +27,30 @@ public class CertificateVersionsController {
 
   static final String API_V1_CERTIFICATE_VERSIONS = "api/v1/certificates/{certificateId}/versions";
 
-  private final EventAuditLogService eventAuditLogService;
   private CertificatesHandler certificatesHandler;
 
   @Autowired
-  public CertificateVersionsController(
-      CertificatesHandler certificateHandler,
-      EventAuditLogService eventAuditLogService) {
+  public CertificateVersionsController(CertificatesHandler certificateHandler) {
     this.certificatesHandler = certificateHandler;
-    this.eventAuditLogService = eventAuditLogService;
   }
 
   @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
   @ResponseStatus(HttpStatus.OK)
   public List<CertificateView> getAllVersions(@PathVariable String certificateId,
-      @RequestParam(value = "current", required = false, defaultValue = "false") boolean current) throws IOException {
-    return eventAuditLogService
-        .auditEvents((auditRecordParameters ->
-            certificatesHandler.handleGetAllVersionsRequest(certificateId, current)
-        ));
+      @RequestParam(value = "current", required = false, defaultValue = "false") boolean current) {
+    return certificatesHandler.handleGetAllVersionsRequest(certificateId, current);
   }
 
   @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
   @ResponseStatus(HttpStatus.OK)
   public CertificateView createVersion(@RequestBody @Validated CreateVersionRequest requestBody,
-      @PathVariable String certificateId) throws IOException {
-    return eventAuditLogService
-        .auditEvents((auditRecordParameters ->
-            certificatesHandler.handleCreateVersionsRequest(certificateId, requestBody)
-        ));
+      @PathVariable String certificateId) {
+    return certificatesHandler.handleCreateVersionsRequest(certificateId, requestBody);
   }
 
   @DeleteMapping(value = "/{versionId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
   @ResponseStatus(HttpStatus.OK)
-  public CertificateView deleteVersion(@PathVariable String certificateId, @PathVariable String versionId) throws IOException {
-    return eventAuditLogService
-        .auditEvents((auditRecordParameters ->
-            certificatesHandler.handleDeleteVersionRequest(certificateId, versionId)
-        ));
+  public CertificateView deleteVersion(@PathVariable String certificateId, @PathVariable String versionId) {
+    return certificatesHandler.handleDeleteVersionRequest(certificateId, versionId);
   }
 }
