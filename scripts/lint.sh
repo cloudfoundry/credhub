7abb85a6d325d5ee0b50cf4869db419593a417bc
Andrew Keesler <akeesler@pivotal.io>
2018-11-30 12:12:24 -0500

    Add ktlint for kotlin
    [#161560539]
    
    Signed-off-by: Walter Scarborough <wscarborough@pivotal.io>

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -14,10 +14,15 @@ function lint_scripts() {
     shellcheck scripts/*
 }
 
+function lint_kotlin() {
+    ./gradlew ktlint
+}
+
 function main() {
     set_bash_error_handling
     go_to_project_root_directory
     lint_scripts
+    lint_kotlin
 }
 
 main "$@"
