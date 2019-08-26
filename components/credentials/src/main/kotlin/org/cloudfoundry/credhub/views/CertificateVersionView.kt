276cfe21652fc322b3c904b9297b4104c1578ac0
Brian Vullo <bvullo@pivotal.io>
2019-08-26 16:26:38 -0400

    fix failing unit test

--- components/credentials/src/main/kotlin/org/cloudfoundry/credhub/views/CertificateVersionView.kt
+++ components/credentials/src/main/kotlin/org/cloudfoundry/credhub/views/CertificateVersionView.kt
@@ -1,6 +1,5 @@
 package org.cloudfoundry.credhub.views
 
-import com.fasterxml.jackson.annotation.JsonGetter
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL
 import com.fasterxml.jackson.annotation.JsonProperty
@@ -9,8 +8,7 @@ import java.util.UUID
 
 class CertificateVersionView(
     val id: UUID,
-    @JsonProperty("expiry_date")
-    val expiryDate: Instant?,
+    expiryDate: Instant?,
     val transitional: Boolean,
     @JsonProperty("certificate_authority")
     val certificateAuthority: Boolean,
@@ -19,8 +17,6 @@ class CertificateVersionView(
     @JsonInclude(NON_NULL)
     val generated: Boolean?
 ) {
-    @JsonGetter
-    fun getExpiryDate(): String {
-        return expiryDate?.toString() ?: ""
-    }
+    @JsonProperty("expiry_date")
+    val expiryDate = expiryDate?.toString() ?: ""
 }
