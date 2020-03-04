444a0d84b94a2554f48d01ff2d7e5a89e50493db
Zayd Zori <zzori@pivotal.io>
2020-03-04 12:28:08 -0500

    add default null name for rsa credential version data
    Signed-off-by: Mia Fryling <mfryling@pivotal.io>

--- components/credentials/src/main/kotlin/org/cloudfoundry/credhub/entity/RsaCredentialVersionData.kt
+++ components/credentials/src/main/kotlin/org/cloudfoundry/credhub/entity/RsaCredentialVersionData.kt
@@ -10,7 +10,7 @@ import org.cloudfoundry.credhub.utils.RsaCredentialHelper
 @Entity
 @DiscriminatorValue(RsaCredentialVersionData.CREDENTIAL_TYPE)
 @SecondaryTable(name = RsaCredentialVersionData.TABLE_NAME, pkJoinColumns = [PrimaryKeyJoinColumn(name = "uuid", referencedColumnName = "uuid")])
-class RsaCredentialVersionData @JvmOverloads constructor(name: String) : CredentialVersionData<RsaCredentialVersionData>(name) {
+class RsaCredentialVersionData @JvmOverloads constructor(name: String? = null) : CredentialVersionData<RsaCredentialVersionData>(name) {
 
     @Column(table = RsaCredentialVersionData.TABLE_NAME, length = 7000)
     var publicKey: String? = null
