688cdd5fc5e189e130c6cd64c90376e0717dcfee
Bruce Ricard <bricard@pivotal.io>
2020-04-29 13:01:42 -0700

    migration classes implement BaseJavaMigration instead of the deprecated SpringJdbcMigration  - change required by Bumping flyway-core from 5.2.4 to 6.4.0
    Signed-off-by: Peter Chen <pchen@pivotal.io>

--- backends/shared-backend-configuration/src/main/kotlin/db/migration/common/V41_1__set_salt_in_existing_user_credentials.kt
+++ backends/shared-backend-configuration/src/main/kotlin/db/migration/common/V41_1__set_salt_in_existing_user_credentials.kt
@@ -7,13 +7,16 @@ import java.sql.ResultSet
 import java.util.UUID
 import org.cloudfoundry.credhub.CryptSaltFactory
 import org.cloudfoundry.credhub.utils.UuidUtil
-import org.flywaydb.core.api.migration.spring.SpringJdbcMigration
+import org.flywaydb.core.api.migration.BaseJavaMigration
+import org.flywaydb.core.api.migration.Context
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.datasource.SingleConnectionDataSource
 
-class V41_1__set_salt_in_existing_user_credentials : SpringJdbcMigration {
+class V41_1__set_salt_in_existing_user_credentials : BaseJavaMigration() {
     @SuppressFBWarnings(value = ["NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE"], justification = "The database will definitely exist")
     @Throws(Exception::class)
-    override fun migrate(jdbcTemplate: JdbcTemplate) {
+    override fun migrate(context: Context) {
+        val jdbcTemplate = JdbcTemplate(SingleConnectionDataSource(context.connection, true))
         val databaseName = jdbcTemplate
             .dataSource
             ?.getConnection()
