fb1d334533cb453138400b48d80c5b35815d190b
Bruce Ricard <bricard@vmware.com>
2020-07-21 15:08:11 -0700

    feat: add logging
    [#173841985](https://www.pivotaltracker.com/story/show/173841985)
    
    Co-authored-by: Aidan Obley <aobley@vmware.com>

--- applications/credhub-api/src/main/java/org/cloudfoundry/credhub/config/FlywayMigrationStrategyConfiguration.java
+++ applications/credhub-api/src/main/java/org/cloudfoundry/credhub/config/FlywayMigrationStrategyConfiguration.java
@@ -1,5 +1,7 @@
 package org.cloudfoundry.credhub.config;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.FlywayException;
 import org.jetbrains.annotations.NotNull;
@@ -10,19 +12,47 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FlywayMigrationStrategyConfiguration {
 
+    private static final Logger LOGGER = LogManager.getLogger(FlywayMigrationStrategyConfiguration.class.getName());
+
     @Bean
     public FlywayMigrationStrategy repairBeforeMigration() {
         return flyway -> {
             repairIfNecessary(flyway);
-            flyway.migrate();
+            runMigration(flyway);
         };
     }
 
     private void repairIfNecessary(@NotNull Flyway flyway) {
         try {
+            LOGGER.info("Validating database state...");
             flyway.validate();
+            LOGGER.info("Validation successful.");
         } catch (FlywayException e) {
-            flyway.repair();
+            try {
+                LOGGER.warn(
+                        String.format(
+                                "Validation failed: \"%s\".",
+                                e.getMessage()
+                        )
+                );
+                LOGGER.info("Attempting to repair...");
+                flyway.repair();
+                LOGGER.info("Repair succeeded.");
+            } catch (FlywayException ex) {
+                LOGGER.fatal("Couldn't repair database. Crashing.");
+                throw ex;
+            }
+        }
+    }
+
+    private void runMigration(@NotNull Flyway flyway) {
+        try {
+            LOGGER.info("Running FlyWay migration....");
+            flyway.migrate();
+            LOGGER.info("FlyWay migration successful.");
+        } catch (FlywayException e) {
+            LOGGER.fatal("FlyWay migration failed. Crashing.");
+            throw e;
         }
     }
 }
