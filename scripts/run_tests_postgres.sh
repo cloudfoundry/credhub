e23a95a3512f6b1ac07d0b44f75b65012b557055
Bruce Ricard <bricard@vmware.com>
2020-09-18 21:55:40 -0700

    feature: connect through TCP
    * otherwise uses socket connection, which doesn't work if the server is for example started in a docker container publishing its port

--- scripts/run_tests_postgres.sh
+++ scripts/run_tests_postgres.sh
@@ -14,16 +14,16 @@ function clean_test_databases_postgres() {
     echo "🛁 Cleaning postgres databases"
     echo ""
 
-    local -r credhub_test_databases=$(psql -U pivotal -c "SELECT datname FROM pg_database WHERE datistemplate = false AND datname LIKE 'credhub_test%';" -t)
+    local -r credhub_test_databases=$(psql -h localhost -U pivotal -c "SELECT datname FROM pg_database WHERE datistemplate = false AND datname LIKE 'credhub_test%';" -t)
 
     for credhub_test_database in $credhub_test_databases; do
         echo "Removing test database: $credhub_test_database"
-        psql -U pivotal -c "DROP DATABASE IF EXISTS $credhub_test_database;"
+        psql -h localhost -U pivotal -c "DROP DATABASE IF EXISTS $credhub_test_database;"
         echo ""
     done
 
     echo "🏗️  Creating postgres test database: credhub_test"
-    psql -U pivotal -c "CREATE DATABASE credhub_test;"
+    psql -h localhost -U pivotal -c "CREATE DATABASE credhub_test;"
     echo ""
 }
 
