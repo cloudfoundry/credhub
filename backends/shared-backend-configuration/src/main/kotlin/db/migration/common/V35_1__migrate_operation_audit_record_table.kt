1062788b8610a7ff2dbe9ac1f7d1d2ae08709aec
Joseph Palermo <jpalermo@pivotal.io>
2021-04-01 16:06:51 -0700

    Upgrade flyway
    This allows support of postgres 13 which has been added to the bosh director
    
    The issue with flyway v4 having a different default schema migrations table name
    than flyway v5+ is resolved with a check that runs before migrating, and if the
    old table is found, it is simply renamed to the new table.
    
    Signed-off-by: Joseph Palermo <jpalermo@pivotal.io>

--- backends/shared-backend-configuration/src/main/kotlin/db/migration/common/V35_1__migrate_operation_audit_record_table.kt
+++ backends/shared-backend-configuration/src/main/kotlin/db/migration/common/V35_1__migrate_operation_audit_record_table.kt
@@ -2,14 +2,17 @@ package db.migration.common
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings
 import org.cloudfoundry.credhub.utils.UuidUtil
-import org.flywaydb.core.api.migration.spring.SpringJdbcMigration
+import org.flywaydb.core.api.migration.BaseJavaMigration
+import org.flywaydb.core.api.migration.Context
 import org.springframework.jdbc.core.JdbcTemplate
 import java.sql.Types
+import org.springframework.jdbc.datasource.SingleConnectionDataSource
 
-class V35_1__migrate_operation_audit_record_table : SpringJdbcMigration {
+class V35_1__migrate_operation_audit_record_table : BaseJavaMigration() {
     @SuppressFBWarnings(value = ["NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE"], justification = "The database will definitely exist")
     @Throws(Exception::class)
-    override fun migrate(jdbcTemplate: JdbcTemplate) {
+    override fun migrate(context: Context) {
+        val jdbcTemplate = JdbcTemplate(SingleConnectionDataSource(context.connection, true))
         val databaseName = jdbcTemplate
             .dataSource
             ?.getConnection()
--- backends/shared-backend-configuration/src/main/kotlin/db/migration/common/V41_1__set_salt_in_existing_user_credentials.kt
+++ backends/shared-backend-configuration/src/main/kotlin/db/migration/common/V41_1__set_salt_in_existing_user_credentials.kt
@@ -3,17 +3,20 @@ package db.migration.common
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings
 import org.cloudfoundry.credhub.CryptSaltFactory
 import org.cloudfoundry.credhub.utils.UuidUtil
-import org.flywaydb.core.api.migration.spring.SpringJdbcMigration
+import org.flywaydb.core.api.migration.BaseJavaMigration
+import org.flywaydb.core.api.migration.Context
 import org.springframework.jdbc.core.JdbcTemplate
 import java.nio.ByteBuffer
 import java.nio.charset.StandardCharsets
 import java.sql.ResultSet
 import java.util.UUID
+import org.springframework.jdbc.datasource.SingleConnectionDataSource
 
-class V41_1__set_salt_in_existing_user_credentials : SpringJdbcMigration {
+class V41_1__set_salt_in_existing_user_credentials : BaseJavaMigration() {
     @SuppressFBWarnings(value = ["NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE"], justification = "The database will definitely exist")
     @Throws(Exception::class)
-    override fun migrate(jdbcTemplate: JdbcTemplate) {
+    override fun migrate(context: Context) {
+        val jdbcTemplate = JdbcTemplate(SingleConnectionDataSource(context.connection, true))
         val databaseName = jdbcTemplate
             .dataSource
             ?.getConnection()
@@ -21,7 +24,7 @@ class V41_1__set_salt_in_existing_user_credentials : SpringJdbcMigration {
             ?.databaseProductName
             ?.toLowerCase()
         val saltFactory = CryptSaltFactory()
-        val uuids = jdbcTemplate.query("select uuid from user_credential") { rowSet: ResultSet, rowNum: Int ->
+        val uuids = jdbcTemplate.query("select uuid from user_credential") { rowSet: ResultSet, _: Int ->
             val uuidBytes = rowSet.getBytes("uuid")
             if ("postgresql" == databaseName) {
                 return@query UUID.fromString(String(uuidBytes, StandardCharsets.UTF_8))
