b788d441c5634e96e82baef1cec64ee263da0ac3
Bruce Ricard <bricard@vmware.com>
2021-12-14 16:47:00 -0600

    Refactor: formatting
    Co-Authored-By: Danny Faught <dfaught@vmware.com>

--- applications/credhub-api/src/main/java/org/cloudfoundry/credhub/config/DatatabaseLayerImpl.java
+++ applications/credhub-api/src/main/java/org/cloudfoundry/credhub/config/DatatabaseLayerImpl.java
@@ -40,7 +40,13 @@ public class DatatabaseLayerImpl implements DatabaseLayer {
     public void updateOldMigrationTableName() throws SQLException {
         LOGGER.info("Renaming 'schema_version' migration table to 'flyway_schema_history'");
         try (Statement stmt = connection.createStatement()) {
-            stmt.execute("ALTER TABLE " + OLD_HISTORY_TABLE_NAME + " RENAME TO " + NEW_HISTORY_TABLE_NAME);
+            stmt.execute(
+                    String.format(
+                            "ALTER TABLE %s RENAME TO %s",
+                            OLD_HISTORY_TABLE_NAME,
+                            NEW_HISTORY_TABLE_NAME
+                    )
+            );
         }
     }
 }
