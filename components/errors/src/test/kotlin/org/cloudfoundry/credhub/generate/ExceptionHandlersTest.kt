ea41779746384300696e1f8ae0d19dabea37af23
Hongchol Sinn <hsinn@vmware.com>
2022-05-18 17:39:15 -0700

    fix: ktlint failures
    - Additionally, changed `var` declarations of immutable variables to `val`.

--- components/errors/src/test/kotlin/org/cloudfoundry/credhub/generate/ExceptionHandlersTest.kt
+++ components/errors/src/test/kotlin/org/cloudfoundry/credhub/generate/ExceptionHandlersTest.kt
@@ -2,17 +2,17 @@ package org.cloudfoundry.credhub.generate
 
 import org.cloudfoundry.credhub.exceptions.EntryNotFoundException
 import org.cloudfoundry.credhub.views.ResponseError
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ExceptionHandlersTest {
 
     @Test
     fun handleNotFoundExceptionWorks() {
-        var handler = ExceptionHandlers()
-        var e = EntryNotFoundException("TEST MESSAGE")
+        val handler = ExceptionHandlers()
+        val e = EntryNotFoundException("TEST MESSAGE")
 
-        var result : ResponseError = handler.handleNotFoundException(e)
+        val result: ResponseError = handler.handleNotFoundException(e)
 
         assertEquals("TEST MESSAGE", result.error)
     }
