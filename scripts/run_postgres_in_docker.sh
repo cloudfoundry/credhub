cdbebe0f40b3002ad4699910f4cf2779bcb7d5c9
Andreas Fleig <afleig@vmware.com>
2022-06-13 20:07:35 +0200

    Add scripts/run_mariadb_in_docker.sh & scripts/run_postgres_in_docker.sh
    To automate and document local DB setup

---
+++ scripts/run_postgres_in_docker.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+set -e -o pipefail
+container_name="credhub-postgres-dev"
+db_names="credhub_test pivotal"
+username="pivotal"
+
+main() {
+  trap "cleanup" EXIT
+
+  setup &
+  setup_pid=$!
+
+  docker run \
+    --name "${container_name}" \
+    --rm \
+    -p 5432:5432 \
+    -e POSTGRES_HOST_AUTH_METHOD=trust \
+    docker.io/library/postgres
+}
+
+cleanup() {
+  echo "Stopping Postgres..."
+  docker kill "${container_name}"
+  kill "$setup_pid" 2>/dev/null
+}
+
+setup() {
+  sleep 1
+  try_connect
+  echo -n "Configuring dev-user and database... "
+  docker exec -it "${container_name}" createuser -U postgres --createdb "${username}"
+  for db_name in ${db_names}
+  do
+    docker exec -it "${container_name}" createdb -U "${username}" "${db_name}"
+  done
+
+  echo "done"
+}
+
+try_connect() {
+  started=1
+  while [[ "$started" != "0" ]]
+  do
+    sleep 1
+    echo "Attempting to connect to Postgres..."
+    set +e
+    docker exec "${container_name}" psql -U postgres -v "ON_ERROR_STOP=1" -c "SELECT 1" >/dev/null
+    started=$?
+    set -e
+  done
+}
+
+main
