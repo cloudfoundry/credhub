2af31cfdbaf68cccb033f9ac40ebedce1b017468
Peter Chen <peter-h.chen@broadcom.com>
2024-02-29 16:45:24 -0800

    wip: fix build (but do not know what broke before)

---
+++ components/encryption/src/test/kotlin/org/cloudfoundry/credhub/utils/TestOAuth2IssuerService.kt
@@ -0,0 +1,13 @@
+package org.cloudfoundry.credhub.utils
+
+import org.cloudfoundry.credhub.auth.OAuth2IssuerService
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Service
+
+@Service
+@Profile("unit-test")
+class TestOAuth2IssuerService : OAuth2IssuerService {
+    override fun getIssuer(): String? {
+        return "https://example.com:8443/oauth/token"
+    }
+}
