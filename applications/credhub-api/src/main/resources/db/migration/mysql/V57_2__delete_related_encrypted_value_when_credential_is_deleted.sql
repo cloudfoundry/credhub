5a88be9e1761ae41fe8106104315b0087e4541c9
Hongchol Sinn <hongchol.sinn@broadcom.com>
2024-03-11 23:35:04 -0700

    fix: Orphaned records in MySQL
    - MySQL trigger to delete associated encrypted_value records when credential_versions are deleted
    - Implemented triggers on both credential and credential_version table because a credential_version record can be deleted not only when a credential is deleted but also directly.
    - This will cause h2 & postgresql test cases to fail until triggers are
      implemented for those databases.
    [#182121168]
    
    Co-authored-by: Bruce Ricard <bruce.ricard@broadcom.com>

--- applications/credhub-api/src/main/resources/db/migration/mysql/V57_2__delete_related_encrypted_value_when_credential_is_deleted.sql
+++ applications/credhub-api/src/main/resources/db/migration/mysql/V57_2__delete_related_encrypted_value_when_credential_is_deleted.sql
@@ -1,59 +1,53 @@
 -- Fix for https://github.com/cloudfoundry/credhub/issues/231
--- Create delete trigger on credential table instead of the tables directly
--- reference encrypted_value table because the trigger does not work for
--- cascade-deleted records in MySQL.
-DROP TRIGGER IF EXISTS tr_credential_deleted;
+
+-- Trigger to delete encrypted_value records that are associated with
+-- passowrd_credential or user_credential when a credential is deleted.
+DROP TRIGGER IF EXISTS tr_credential_version_pre_del;
 DELIMITER |
-CREATE TRIGGER tr_credential_deleted BEFORE DELETE ON credential
+CREATE TRIGGER tr_credential_version_pre_del BEFORE DELETE ON credential_version
 FOR EACH ROW
 BEGIN
-    DECLARE done INT DEFAULT FALSE;
-    DECLARE cred_ver_id, enc_val_id BINARY(16);
-    DECLARE cred_ver_type VARCHAR(31);
-    DECLARE cur CURSOR FOR
-        SELECT type, uuid FROM credential_version
-            WHERE credential_uuid = OLD.uuid;
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+    DECLARE enc_val_id BINARY(16);
 
-    OPEN cur;
-    read_loop: LOOP
-        FETCH cur into cred_ver_type, cred_ver_id;
-        IF done THEN
-          LEAVE read_loop;
-        END IF;
-
-        -- For each talbe record that is referencing encrypted_value record,
-        -- set FKs to encrypted_value to null before deleting the encrypted_value
-        -- record. Otherwise, the referencing record cannot be deleted because of
-        -- the FK constraint.
-
-        -- Delete the encrypted_value record that is associated with the
-        -- credential_version record.
-        SELECT encrypted_value_uuid FROM credential_version
-            WHERE uuid = cred_ver_id INTO enc_val_id;
-        UPDATE credential_version SET encrypted_value_uuid = NULL
-            WHERE uuid = cred_ver_id;
+    -- For each record that is referencing encrypted_value record,
+    -- set FKs to encrypted_value to null before deleting the encrypted_value
+    -- record. Otherwise, the referencing record cannot be deleted because of
+    -- the FK constraint.
+    IF OLD.type = 'password' THEN
+        SELECT password_parameters_uuid from password_credential
+            WHERE uuid = OLD.uuid INTO enc_val_id;
+        UPDATE password_credential SET password_parameters_uuid = NULL
+            WHERE uuid = OLD.uuid;
         DELETE FROM encrypted_value WHERE uuid = enc_val_id;
-        SET enc_val_id = NULL;
-
-        -- For password or user type credential, the password_credential
-        -- or user_credential record also has associated encrypted_value record.
-        -- Delete them, too.
-        IF cred_ver_type = 'password' THEN
-            SELECT password_parameters_uuid from password_credential
-                WHERE uuid = cred_ver_id INTO enc_val_id;
-            UPDATE password_credential SET password_parameters_uuid = NULL
-                WHERE uuid = cred_ver_id;
-            DELETE FROM encrypted_value WHERE uuid = enc_val_id;
-        ELSEIF cred_ver_type = 'user' THEN
-            SELECT password_parameters_uuid from user_credential
-                WHERE uuid = cred_ver_id INTO enc_val_id;
-            UPDATE user_credential SET password_parameters_uuid = NULL
-                WHERE uuid = cred_ver_id;
-            DELETE FROM encrypted_value WHERE uuid = enc_val_id;
-        END IF;
-    END LOOP read_loop;
-    CLOSE cur;
+    ELSEIF OLD.type = 'user' THEN
+        SELECT password_parameters_uuid from user_credential
+            WHERE uuid = OLD.uuid INTO enc_val_id;
+        UPDATE user_credential SET password_parameters_uuid = NULL
+            WHERE uuid = OLD.uuid;
+        DELETE FROM encrypted_value WHERE uuid = enc_val_id;
+    END IF;
 END;
 |
 DELIMITER ;
+
+-- Trigger to delete the encrypted_values records that are associated
+-- with the credential_version record when a credential_version was
+-- deleted.
+DROP TRIGGER IF EXISTS tr_credential_version_post_del;
+CREATE TRIGGER tr_credential_version_post_del AFTER DELETE ON credential_version
+FOR EACH ROW
+    -- Delete the encrypted_value record that is associated with the
+    -- credential_version record.
+    DELETE FROM encrypted_value WHERE uuid = OLD.encrypted_value_uuid;
+
+-- Trigger to delete all associated encrypted_value records when a credential
+-- is deleted. Needed to define one on credential table to work around the MySQL
+-- trigger limitation where the triggers on cascade-deleted records are not
+-- executed.
+DROP TRIGGER IF EXISTS tr_credential_pre_del;
+CREATE TRIGGER tr_credential_pre_del BEFORE DELETE ON credential
+FOR EACH ROW
+    -- Delete the child record here instead of relying on casecasde-delete, as
+    -- MySQL trigger on the child table does not get executed when
+    -- cascade-deleteed.
+    DELETE FROM credential_version WHERE credential_uuid = OLD.uuid;
