2c19cfab763a4976d311948a5bca9de01852e867
Hongchol Sinn <hongchol.sinn@broadcom.com>
2024-05-31 16:26:00 -0700

    Build the index without taking any locks
    [#187452409]

--- applications/credhub-api/src/main/resources/db/migration/postgres/V59_1__index_credential_version_credential_uuid.sql
+++ applications/credhub-api/src/main/resources/db/migration/postgres/V59_1__index_credential_version_credential_uuid.sql
@@ -1,2 +1,2 @@
-CREATE INDEX IF NOT EXISTS idx_credential_version_credential_uuid
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_credential_version_credential_uuid
     ON credential_version(credential_uuid);
