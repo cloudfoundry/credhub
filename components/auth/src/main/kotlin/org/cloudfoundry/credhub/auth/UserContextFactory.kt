045261878277711de1f3856908caa9f8a22a5767
Hongchol Sinn <hongchol.sinn@broadcom.com>
2025-02-12 14:37:34 -0800

    feat: Replacing dependencies to old ouath2 lib `AuthConfiguration`
    - Rewrote the class in Java, replacing the use of the old oauth2 lib with new lib.
    - Made all test cases except 4 mTLS-related ones pass in `AuthConfigurationTest`.
    - Have not tried to run the production code, i.e. run the credhub server application yet.
    
    Co-authored-by: Joe Eltgroth <joe.eltgroth@broadcom.com>

--- components/auth/src/main/kotlin/org/cloudfoundry/credhub/auth/UserContextFactory.kt
+++ components/auth/src/main/kotlin/org/cloudfoundry/credhub/auth/UserContextFactory.kt
@@ -2,10 +2,8 @@ package org.cloudfoundry.credhub.auth
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.Authentication
-import org.springframework.security.oauth2.common.OAuth2AccessToken
-import org.springframework.security.oauth2.provider.OAuth2Authentication
-import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails
-import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices
+import org.springframework.security.oauth2.jwt.Jwt
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken
 import org.springframework.stereotype.Component
 import java.security.cert.X509Certificate
@@ -13,8 +11,7 @@ import java.security.cert.X509Certificate
 @Component
 class UserContextFactory {
     @Autowired(required = false)
-    private val resourceServerTokenServices: ResourceServerTokenServices? = null
-
+//    private val resourceServerTokenServices: ResourceServerTokenServices? = null
     /*
      * The "iat" and "exp" claims are parsed by Jackson as integers,
      * because JWT defines these as seconds since Epoch
@@ -22,52 +19,56 @@ class UserContextFactory {
      * Year-2038 bug. To adapt to our local model, hoping JWT will some day be improved,
      * this function returns a numeric value as long.
      */
-    private fun claimValueAsLong(additionalInformation: Map<String, Any>): Long = (additionalInformation["iat"] as Number).toLong()
+    private fun claimValueAsLong(additionalInformation: Map<String, Any>): Long =
+        (additionalInformation["iat"] as Number).toLong()
 
     fun createUserContext(authentication: Authentication?): UserContext =
         if (authentication is PreAuthenticatedAuthenticationToken) {
             createUserContext(authentication)
         } else {
-            createUserContext(authentication as OAuth2Authentication, null)
+            createUserContext(authentication as JwtAuthenticationToken, null)
         }
 
     fun createUserContext(
-        authentication: OAuth2Authentication,
+        authentication: JwtAuthenticationToken,
         maybeToken: String?,
     ): UserContext {
-        val oauth2Request = authentication.oAuth2Request
-        val clientId = oauth2Request.clientId
-        val grantType = oauth2Request.grantType
-        var userId: String? = null
-        var userName: String? = null
-        var issuer: String? = null
-        var validFrom: Long = 0
-        var validUntil: Long = 0
-        lateinit var scope: String
+        val jwt: Jwt = authentication.principal as Jwt
+        val claims = jwt.claims
+        val grantType = claims["grant_type"] as String
+        val clientId = claims["client_id"] as String
+        val userId = claims["user_id"] as String?
+        val userName = claims["user_name"] as String?
+        val issuer = claims["iss"] as String?
+        val validFrom: Long = 0
+        val validUntil: Long = 0
+        val scopes = claims["scope"] as List<*>
+        val scope = scopes.joinToString(",")
 
-        var token = maybeToken
+        // TODO: Will other tests fail without the below?
+//        var token = maybeToken
+//
+//        if (maybeToken == null) {
+//            val authDetails =
+//                authentication
+//                    .details as OAuth2AuthenticationDetails
+//            token = authDetails.tokenValue
+//        }
 
-        if (maybeToken == null) {
-            val authDetails =
-                authentication
-                    .details as OAuth2AuthenticationDetails
-            token = authDetails.tokenValue
-        }
-
-        val accessToken: OAuth2AccessToken?
-        accessToken = resourceServerTokenServices!!.readAccessToken(token)
-
-        if (accessToken != null) {
-            val scopes = accessToken.scope
-            scope = scopes.joinToString(",")
-
-            val additionalInformation = accessToken.additionalInformation
-            userName = additionalInformation["user_name"] as String?
-            userId = additionalInformation["user_id"] as String?
-            issuer = additionalInformation["iss"] as String?
-            validFrom = claimValueAsLong(additionalInformation)
-            validUntil = accessToken.expiration.toInstant().epochSecond
-        }
+//        val accessToken: OAuth2AccessToken?
+//        accessToken = resourceServerTokenServices!!.readAccessToken(token)
+//
+//        if (accessToken != null) {
+//            val scopes = accessToken.scope
+//            scope = scopes.joinToString(",")
+//
+//            val additionalInformation = accessToken.additionalInformation
+//            userName = additionalInformation["user_name"] as String?
+//            userId = additionalInformation["user_id"] as String?
+//            issuer = additionalInformation["iss"] as String?
+//            validFrom = claimValueAsLong(additionalInformation)
+//            validUntil = accessToken.expiration.toInstant().epochSecond
+//        }
 
         return UserContext(
             userId,
