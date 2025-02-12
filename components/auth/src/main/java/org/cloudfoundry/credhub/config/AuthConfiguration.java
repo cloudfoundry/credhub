6563e12bdc39d91211957a2499cd896413aeaca0
Hongchol Sinn <hongchol.sinn@broadcom.com>
2025-02-12 14:37:34 -0800

    feat: Replacing dependencies to old ouath2 lib `AuthConfiguration`
    - Rewrote the class in Java, replacing the use of the old oauth2 lib with new lib.
    - Made all test cases except 4 mTLS-related ones pass in `AuthConfigurationTest`.
    - Have not tried to run the production code, i.e. run the credhub server application yet.
    
    Co-authored-by: Joe Eltgroth <joe.eltgroth@broadcom.com>

---
+++ components/auth/src/main/java/org/cloudfoundry/credhub/config/AuthConfiguration.java
@@ -0,0 +1,122 @@
+package org.cloudfoundry.credhub.config;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.cloudfoundry.credhub.auth.ActuatorPortFilter;
+import org.cloudfoundry.credhub.auth.PreAuthenticationFailureFilter;
+import org.cloudfoundry.credhub.auth.X509AuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.AuthorityAuthorizationManager;
+import org.springframework.security.authorization.AuthorizationManagers;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider;
+import org.springframework.security.web.authentication.preauth.x509.X509AuthenticationFilter;
+
+import java.security.interfaces.RSAPublicKey;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@ConditionalOnProperty("security.oauth2.enabled")
+@Configuration
+@EnableWebSecurity
+public class AuthConfiguration {
+    private static final String VALID_MTLS_ID = "\\bOU=(app:[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[a-f0-9]{4}-[a-f0-9]{12})\\b";
+    private static final Logger LOGGER = LogManager.getLogger(AuthConfiguration.class.getName());
+
+    @Value("${security.oauth2.resource.jwt.key_value}")
+    RSAPublicKey publicKey;
+
+    @Autowired
+    OAuthProperties oAuthProperties;
+
+    @Autowired
+    ActuatorPortFilter actuatorPortFilter;
+
+    @Autowired
+    PreAuthenticationFailureFilter preAuthenticationFailureFilter;
+
+    @Autowired
+    OAuth2ExtraValidationFilter oAuth2ExtraValidationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+         LOGGER.info("in securityFilterChain config");
+
+        http
+                .x509()
+                .subjectPrincipalRegex(VALID_MTLS_ID)
+                .userDetailsService(mtlsSUserDetailsService());
+
+        http
+                .addFilterBefore(actuatorPortFilter, X509AuthenticationFilter.class)
+                .addFilterAfter(preAuthenticationFailureFilter, X509AuthenticationFilter.class)
+                .addFilterAfter(oAuth2ExtraValidationFilter, PreAuthenticationFailureFilter.class)
+                .authenticationProvider(preAuthenticatedAuthenticationProvider());
+
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/info").permitAll()
+                        .requestMatchers("/docs/index.html").permitAll()
+                        .requestMatchers("/health").permitAll()
+                        .requestMatchers("/management").permitAll()
+                        .requestMatchers("/**").access(
+                                AuthorizationManagers.anyOf(
+                                        AuthorizationManagers.allOf(
+                                                AuthorityAuthorizationManager.hasAuthority("SCOPE_credhub.read"),
+                                                AuthorityAuthorizationManager.hasAuthority("SCOPE_credhub.write")
+                                        ),
+                                        // TODO: Use X509AuthenticationProvider.MTLS_USER instead of the literal.
+                                        AuthorityAuthorizationManager.hasRole("MTLS_USER"))
+                                )
+                )
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(withDefaults()))
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() throws Exception {
+        LOGGER.info("in jwtDecoder");
+        return NimbusJwtDecoder.withPublicKey(publicKey).build();
+
+// TODO: The code we want to use for UAA providing the public key
+//        String jwkKeysPath = "/token_keys";
+//        try {
+//            jwkKeysPath = oAuthProperties.getJwkKeysPath();
+//        } catch (URISyntaxException ex) {
+//            LOGGER.warn("Using default jwkKeysPath: {}", jwkKeysPath);
+//        }
+//
+//        return NimbusJwtDecoder.withJwkSetUri(jwkKeysPath).build();
+    }
+
+    private UserDetailsService mtlsSUserDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return new User(username, "", AuthorityUtils.NO_AUTHORITIES);
+            }
+        };
+    }
+
+    private PreAuthenticatedAuthenticationProvider preAuthenticatedAuthenticationProvider() {
+        return new X509AuthenticationProvider();
+    }
+}
