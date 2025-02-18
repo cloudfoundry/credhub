52eb1e3a41439b49d89d2b0aaf6108d1352cf21a
Joe Eltgroth <joe.eltgroth@broadcom.com>
2025-02-18 13:09:00 -0600

    fix: Make UserContextFactoryTest pass

--- components/auth/src/test/kotlin/org/cloudfoundry/credhub/auth/UserContextFactoryTest.kt
+++ components/auth/src/test/kotlin/org/cloudfoundry/credhub/auth/UserContextFactoryTest.kt
@@ -6,20 +6,16 @@ import org.cloudfoundry.credhub.auth.UserContext.Companion.AUTH_METHOD_UAA
 import org.cloudfoundry.credhub.utils.DatabaseProfileResolver
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.core.IsEqual.equalTo
+import org.hamcrest.core.StringContains.containsString
 import org.junit.Test
 import org.junit.jupiter.api.Assertions
 import org.junit.runner.RunWith
 import org.mockito.Mockito.mock
-import org.mockito.Mockito.spy
 import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.security.oauth2.common.OAuth2AccessToken
-import org.springframework.security.oauth2.provider.OAuth2Authentication
-import org.springframework.security.oauth2.provider.OAuth2Request
-import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails
-import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices
+import org.springframework.security.oauth2.jwt.Jwt
+import org.springframework.security.oauth2.jwt.JwtClaimNames
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken
 import org.springframework.test.context.ActiveProfiles
@@ -28,16 +24,11 @@ import java.security.Principal
 import java.security.cert.X509Certificate
 import java.time.Instant
 import java.util.Date
-import java.util.HashMap
-import java.util.HashSet
 
 @RunWith(SpringRunner::class)
 @ActiveProfiles(profiles = ["unit-test"], resolver = DatabaseProfileResolver::class)
 @SpringBootTest(classes = [CredhubTestApp::class])
 class UserContextFactoryTest {
-    @MockBean
-    private val tokenServicesMock: ResourceServerTokenServices? = null
-
     @Autowired
     private val subject: UserContextFactory? = null
 
@@ -57,19 +48,18 @@ class UserContextFactoryTest {
         assertThat<String>(context.authMethod, equalTo(AUTH_METHOD_UAA))
     }
 
-    // TODO: Commented out because of compilation failure. Uncomment and make it work.
-//    @Test
-//    @Throws(Exception::class)
-//    fun fromAuthentication_handlesSuppliedToken() {
-//        val oauth2Authentication = setupOAuthMock("TEST_GRANT_TYPE")
-//
-//        val context = subject!!.createUserContext(oauth2Authentication, "tokenValue")
-//
-//        assertThat<String>(context.userName, equalTo("TEST_USER_NAME"))
-//        assertThat<String>(context.issuer, containsString("TEST_UAA_URL"))
-//        assertThat<String>(context.scope, equalTo("scope1,scope2"))
-//        assertThat<String>(context.authMethod, equalTo(AUTH_METHOD_UAA))
-//    }
+    @Test
+    @Throws(Exception::class)
+    fun fromAuthentication_handlesSuppliedToken() {
+        val oauth2Authentication = setupOAuthMock("TEST_GRANT_TYPE")
+
+        val context = subject!!.createUserContext(oauth2Authentication)
+
+        assertThat<String>(context.userName, equalTo("TEST_USER_NAME"))
+        assertThat<String>(context.issuer, containsString("TEST_UAA_URL"))
+        assertThat<String>(context.scope, equalTo("scope1,scope2"))
+        assertThat<String>(context.authMethod, equalTo(AUTH_METHOD_UAA))
+    }
 
     @Test
     @Throws(Exception::class)
@@ -160,49 +150,28 @@ class UserContextFactoryTest {
         }
     }
 
-    private fun setupOAuthMock(grantType: String): OAuth2Authentication {
+    private fun setupOAuthMock(grantType: String): JwtAuthenticationToken {
         val authentication = mock(JwtAuthenticationToken::class.java)
-        val oauth2Request =
-            spy(
-                OAuth2Request(
-                    null,
-                    "TEST_CLIENT_ID",
-                    null,
-                    false,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                ),
+        val jwt = mock(Jwt::class.java)
+        val scopeList = listOf("scope1", "scope2")
+
+        val claims =
+            mapOf(
+                JwtClaimNames.SUB to "TEST_USER_ID",
+                JwtClaimNames.ISS to "TEST_UAA_URL",
+                "user_id" to "TEST_USER_ID",
+                "user_name" to "TEST_USER_NAME",
+                "grant_type" to grantType,
+                "client_id" to "TEST_CLIENT_ID",
+                JwtClaimNames.IAT to Instant.ofEpochMilli(1413495264),
+                JwtClaimNames.EXP to Instant.ofEpochMilli(1413538464),
+                "scope" to scopeList,
             )
-        val token = mock(OAuth2AccessToken::class.java)
-        val authDetails = mock(OAuth2AuthenticationDetails::class.java)
 
-        val additionalInformation = HashMap<String, Any>()
-        additionalInformation["user_id"] = "TEST_USER_ID"
-        additionalInformation["user_name"] = "TEST_USER_NAME"
-        additionalInformation["iss"] = "TEST_UAA_URL"
-        additionalInformation["iat"] = 1413495264
-
-        val scopes = HashSet<String>()
-        scopes.add("scope1")
-        scopes.add("scope2")
-
-        `when`(oauth2Request.grantType).thenReturn(grantType)
-        `when`(authentication.details).thenReturn(authDetails)
-        `when`(authDetails.tokenValue).thenReturn("tokenValue")
-
-//        `when`(authentication.oAuth2Request).thenReturn(oauth2Request)
-        `when`(token.additionalInformation).thenReturn(additionalInformation)
-        `when`(token.expiration).thenReturn(Date.from(Instant.ofEpochSecond(1413538464)))
-        `when`(token.scope).thenReturn(scopes)
-
-        `when`(tokenServicesMock!!.readAccessToken("tokenValue")).thenReturn(token)
-
-        // TODO: Temp change for type-misnatch compilation error. Undo to make it work.
-//        return authentication
-        return mock(OAuth2Authentication::class.java)
+        `when`(jwt.claims).thenReturn(claims)
+        `when`(jwt.subject).thenReturn("TEST_USER_ID")
+        `when`(authentication.principal).thenReturn(jwt)
+        return authentication
     }
 
     private fun setupMtlsMock(): PreAuthenticatedAuthenticationToken {
