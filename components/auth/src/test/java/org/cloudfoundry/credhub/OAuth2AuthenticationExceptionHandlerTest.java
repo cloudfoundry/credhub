768d7e99b6f99b910701fa1efa8de5a7f632a5e5
Joe Eltgroth <joe.eltgroth@broadcom.com>
2025-03-14 15:41:03 -0500

    Include InsufficientAuthenticationException in the response body
    Co-Authored-By: Hongchol Sinn <hongchol.sinn@broadcom.com>

---
+++ components/auth/src/test/java/org/cloudfoundry/credhub/OAuth2AuthenticationExceptionHandlerTest.java
@@ -0,0 +1,53 @@
+package org.cloudfoundry.credhub;
+
+import org.cloudfoundry.credhub.config.auth.OAuth2AuthenticationExceptionHandler;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2Error;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OAuth2AuthenticationExceptionHandlerTest {
+
+    private OAuth2AuthenticationExceptionHandler exceptionHandler;
+    private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
+
+    @Before
+    public void setUp() {
+        exceptionHandler = new OAuth2AuthenticationExceptionHandler();
+        request = new MockHttpServletRequest();
+        response = new MockHttpServletResponse();
+    }
+
+    @Test
+    public void testOAuth2AuthenticationExceptionHandling() throws Exception {
+        OAuth2Error error = new OAuth2Error
+                ("error", "error-description", "error-uri");
+        OAuth2AuthenticationException oae =
+                new OAuth2AuthenticationException(error, "description");
+
+        exceptionHandler.commence(request, response, oae);
+
+        assertEquals(
+                "{\"error\":\"error\",\"error_description\":\"error-description\"}",
+                response.getContentAsString());
+    }
+
+
+    @Test
+    public void testInsufficientAuthenticationExceptionHandling() throws Exception {
+        InsufficientAuthenticationException iae
+                = new InsufficientAuthenticationException("Full authentication required");
+
+        exceptionHandler.commence(request, response, iae);
+
+        assertEquals(
+                "{\"error_description\":\"Full authentication required\"}",
+                response.getContentAsString());
+    }
+}
