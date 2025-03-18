2faaf2986b8da9496a04df3f8f719e0a730e9886
Joe Eltgroth <joe.eltgroth@broadcom.com>
2025-03-18 15:28:04 -0500

    fix: Make the InsufficientAuthenticationException handling handle the mtls_test.go acceptance test expectations.

--- components/auth/src/test/java/org/cloudfoundry/credhub/config/auth/OAuth2AuthenticationExceptionHandlerTest.java
+++ components/auth/src/test/java/org/cloudfoundry/credhub/config/auth/OAuth2AuthenticationExceptionHandlerTest.java
@@ -47,7 +47,7 @@ public class OAuth2AuthenticationExceptionHandlerTest {
         exceptionHandler.commence(request, response, iae);
 
         assertEquals(
-                "{\"error_description\":\"Full authentication required\"}",
+                "{\"error\":\"invalid_token\",\"error_description\":\"Full authentication required\"}",
                 response.getContentAsString());
     }
 }
