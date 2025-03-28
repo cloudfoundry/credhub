d5d3d9435ac031616bb6894db426423b2f26535e
Joe Eltgroth <joe.eltgroth@broadcom.com>
2025-03-28 09:15:43 -0500

    Fix Unresolved reference 'HandlerInterceptorAdapter'

--- components/audit/src/main/kotlin/org/cloudfoundry/credhub/interceptors/AuditInterceptor.kt
+++ components/audit/src/main/kotlin/org/cloudfoundry/credhub/interceptors/AuditInterceptor.kt
@@ -7,9 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.lang.Nullable
 import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Component
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.web.servlet.HandlerInterceptor
 
 @Component
 class AuditInterceptor
@@ -17,7 +17,7 @@ class AuditInterceptor
     internal constructor(
         private val userContextFactory: UserContextFactory,
         private val auditRecord: CEFAuditRecord,
-    ) : HandlerInterceptorAdapter() {
+    ) : HandlerInterceptor {
         override fun preHandle(
             request: HttpServletRequest,
             response: HttpServletResponse,
