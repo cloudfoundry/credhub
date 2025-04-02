9dc8984da51286b7428fadcc749b4f439d877dae
Joe Eltgroth <joe.eltgroth@broadcom.com>
2025-04-02 16:25:40 -0500

    Misc linting and deprecation cleanup

--- components/http-support/src/main/kotlin/org/cloudfoundry/credhub/RestTemplateFactory.kt
+++ components/http-support/src/main/kotlin/org/cloudfoundry/credhub/RestTemplateFactory.kt
@@ -1,8 +1,8 @@
 package org.cloudfoundry.credhub
 
-import org.apache.hc.client5.http.ssl.ClientTlsStrategyBuilder
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder
+import org.apache.hc.client5.http.ssl.ClientTlsStrategyBuilder
 import org.apache.hc.client5.http.ssl.TlsSocketStrategy
 import org.apache.hc.core5.ssl.SSLContextBuilder
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
@@ -33,10 +33,11 @@ class RestTemplateFactory {
             SSLContextBuilder()
                 .loadTrustMaterial(trustStore, trustStorePassword.toCharArray())
                 .build()
-        val tlsSocketStrategy: TlsSocketStrategy = ClientTlsStrategyBuilder
-            .create()
-            .setSslContext(sslContext)
-            .build() as TlsSocketStrategy
+        val tlsSocketStrategy: TlsSocketStrategy =
+            ClientTlsStrategyBuilder
+                .create()
+                .setSslContext(sslContext)
+                .build() as TlsSocketStrategy
 
         val httpClient =
             HttpClientBuilder
