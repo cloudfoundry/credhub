42aa120faa54217e6c29986b44da6c4ab8fc8748
Joe Eltgroth <joe.eltgroth@broadcom.com>
2025-04-10 13:15:40 -0500

    Try to fix UuidGenerator

--- components/encryption/src/main/java/org/cloudfoundry/credhub/entities/UuidGenerator.java
+++ components/encryption/src/main/java/org/cloudfoundry/credhub/entities/UuidGenerator.java
@@ -10,9 +10,8 @@ import org.hibernate.id.UUIDGenerator;
 public class UuidGenerator extends UUIDGenerator {
   @Override
   public Serializable generate(final SharedSessionContractImplementor session, final Object object) throws HibernateException {
-    final Serializable uuid = session.getEntityPersister(null, object)
-      .getClassMetadata().getIdentifier(object, session);
+    final Serializable uuid = (Serializable) session.getEntityPersister(null, object).getIdentifier(object, session);
 
-    return uuid != null ? uuid : super.generate(session, object);
+    return uuid != null ? uuid : (Serializable) super.generate(session, object);
   }
 }
