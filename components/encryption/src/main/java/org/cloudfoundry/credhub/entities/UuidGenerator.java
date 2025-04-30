58fcbb506afbc386d6418b993a6312fb79e3746e
Hongchol Sinn <hongchol.sinn@broadcom.com>
2025-04-30 15:55:34 -0700

    fix: UuidGeneratorTest.generate_ifUUIDSet_itKeepsUUID() failure - The test case failed with latest hibernate version as follows for example: ``` org.hibernate.StaleObjectStateException: Row was updated or deleted by another transaction (or unsaved-value mapping was incorrect): [org.cloudfoundry.credhub.entities.EncryptionKeyCanary#0a599160-88af-4faa-b664-95d4166d9c33] ``` - Latest hibernate version disallows manually setting generated identifier value by default. - Overrode the default behavior of `Generator` so we can continue setting `EncryptionKeyCanary.uuid` manually, as suggested at https://discourse.hibernate.org/t/manually-setting-the-identifier-results-in-object-optimistic-locking-failure-exception/10743.

--- components/encryption/src/main/java/org/cloudfoundry/credhub/entities/UuidGenerator.java
+++ components/encryption/src/main/java/org/cloudfoundry/credhub/entities/UuidGenerator.java
@@ -7,6 +7,11 @@ import org.hibernate.id.UUIDGenerator;
 @SuppressWarnings("unused")
 public class UuidGenerator extends UUIDGenerator {
   @Override
+  public boolean allowAssignedIdentifiers() {
+    return true;
+  }
+
+  @Override
   public Object generate(final SharedSessionContractImplementor session, final Object object) throws HibernateException {
     final Object uuid = session.getEntityPersister(null, object)
       .getClassMetadata().getIdentifier(object, session);
